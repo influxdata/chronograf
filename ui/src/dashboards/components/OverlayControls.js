2df2f76610a48054fb960a8ef83d55b8f27c07ce
Jared Scheib <jared.scheib@gmail.com>
2017-03-14 15:27:19 -0700

    Add markup and styles for OverlayControls

--- ui/src/dashboards/components/OverlayControls.js
+++ ui/src/dashboards/components/OverlayControls.js
@@ -3,7 +3,19 @@ import React from 'react'
 import ConfirmButtons from 'src/admin/components/ConfirmButtons'
 
 const OverlayControls = () => (
-  <ConfirmButtons />
+  <div className="overlay-controls">
+    <h3>Graph</h3>
+    <div>
+      <h4>Type:</h4>
+      <ul>
+        <li>Line</li>
+        <li>Stacked</li>
+        <li>Step-plot</li>
+        <li>SingleStat</li>
+      </ul>
+    </div>
+    <ConfirmButtons />
+  </div>
 )
 
 export default OverlayControls
