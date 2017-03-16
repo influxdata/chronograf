21598e45ca54bafed8a46cb70213f0d81a8183fc
Jared Scheib <jared.scheib@gmail.com>
2017-03-15 17:42:10 -0700

    Refactor CellEditorOverlay to allow selection of graph type

--- ui/src/dashboards/components/OverlayControls.js
+++ ui/src/dashboards/components/OverlayControls.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
+import classnames from 'classnames'
 
 import ConfirmButtons from 'src/admin/components/ConfirmButtons'
 
-const OverlayControls = () => (
-  <div className="overlay-controls">
-    <h3>Graph</h3>
-    <div>
-      <h4>Type:</h4>
-      <ul>
-        <li>Line</li>
-        <li>Stacked</li>
-        <li>Step-plot</li>
-        <li>SingleStat</li>
-      </ul>
+const OverlayControls = (props) => {
+  const {graphTypes, selectedGraphType, onSelectGraphType} = props
+  return (
+    <div className="overlay-controls">
+      <h3>Graph</h3>
+      <div>
+        <h4>Type:</h4>
+        <ul>
+          {graphTypes.map(type =>
+            <li
+              key={type}
+              className={classnames('radioItem', {selected: type === selectedGraphType})}
+              onClick={() => onSelectGraphType(type)}>{type}
+            </li>
+          )}
+        </ul>
+      </div>
+      <ConfirmButtons />
     </div>
-    <ConfirmButtons />
-  </div>
-)
+  )
+}
+
+const {
+  arrayOf,
+  func,
+  string,
+} = PropTypes
+
+OverlayControls.propTypes = {
+  graphTypes: arrayOf(string).isRequired,
+  selectedGraphType: string.isRequired,
+  onSelectGraphType: func.isRequired,
+}
 
 export default OverlayControls
