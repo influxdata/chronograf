68281ce9653df334ed80c179f32eaa6ba1b8586c
Alex P <thealexpaxton@gmail.com>
2017-03-16 12:24:17 -0700

    Style Overlay controls, move confirm buttons to own stylesheet

--- ui/src/dashboards/components/OverlayControls.js
+++ ui/src/dashboards/components/OverlayControls.js
@@ -4,17 +4,17 @@ import ConfirmButtons from 'src/admin/components/ConfirmButtons'
 
 const OverlayControls = () => (
   <div className="overlay-controls">
-    <h3>Graph</h3>
-    <div>
-      <h4>Type:</h4>
-      <ul>
-        <li>Line</li>
-        <li>Stacked</li>
-        <li>Step-plot</li>
-        <li>SingleStat</li>
+    <h3 className="overlay--graph-name">Graph Editor</h3>
+    <div className="overlay-controls--right">
+      <p>Visualization Type:</p>
+      <ul className="toggle toggle-sm">
+        <li className="toggle-btn active">Line</li>
+        <li className="toggle-btn">Stacked</li>
+        <li className="toggle-btn">Step-plot</li>
+        <li className="toggle-btn">SingleStat</li>
       </ul>
+      <ConfirmButtons />
     </div>
-    <ConfirmButtons />
   </div>
 )
 
