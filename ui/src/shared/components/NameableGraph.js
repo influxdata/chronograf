26d6ca4b697da09ab46220b43424b253a0570ccd
Tim Raymond <tim@timraymond.com>
2017-03-22 10:42:52 -0400

    Convert NameableGraph to createClass for state
    This was converted from a pure function to encapsulate the state of the
    buttons. An attempt was made previously to store this state in Redux,
    but it proved too convoluted with the current state of the reducers for
    cells and dashboards. Another effort must take place to separate a cell
    reducer to manage the state of an individual cell in Redux in order for
    this state to be sanely kept in Redux as well.
    
    For the time being, this state is being kept in the component for the
    sake of expeditiousness, since this is needed for Dashboards to be
    released. A refactor of this will occur later.

--- ui/src/shared/components/NameableGraph.js
+++ ui/src/shared/components/NameableGraph.js
@@ -1,61 +1,7 @@
 import React, {PropTypes} from 'react'
 
-const NameableGraph = ({
-  cell: {
-    name,
-    isEditing,
-    x,
-    y,
-  },
-  cell,
-  children,
-  onEditCell,
-  onRenameCell,
-  onUpdateCell,
-}) => {
-  let nameOrField
-  const isEditable = !!(onEditCell || onRenameCell || onUpdateCell)
-
-  if (isEditing && isEditable) {
-    nameOrField = (
-      <input
-        className="form-control input-sm dash-graph--name-edit"
-        type="text"
-        value={name}
-        autoFocus={true}
-        onChange={onRenameCell(x, y)}
-        onBlur={onUpdateCell(cell)}
-        onKeyUp={(evt) => {
-          if (evt.key === 'Enter') {
-            onUpdateCell(cell)()
-          }
-        }}
-      />
-    )
-  } else {
-    nameOrField = (<span className="dash-graph--name">{name}</span>)
-  }
-
-  let onClickHandler
-  if (isEditable) {
-    onClickHandler = onEditCell
-  } else {
-    onClickHandler = () => {
-      // no-op
-    }
-  }
-
-  return (
-    <div>
-      <h2 className="dash-graph--heading" onClick={onClickHandler(x, y, isEditing)}>{nameOrField}</h2>
-      <div className="dash-graph--container">
-        {children}
-      </div>
-    </div>
-  )
-}
-
 const {
+  bool,
   func,
   node,
   number,
@@ -63,16 +9,109 @@ const {
   string,
 } = PropTypes
 
-NameableGraph.propTypes = {
-  cell: shape({
-    name: string.isRequired,
-    x: number.isRequired,
-    y: number.isRequired,
-  }).isRequired,
-  children: node.isRequired,
-  onEditCell: func,
-  onRenameCell: func,
-  onUpdateCell: func,
-}
+const NameableGraph = React.createClass({
+  propTypes: {
+    cell: shape({
+      name: string.isRequired,
+      isEditing: bool,
+      x: number.isRequired,
+      y: number.isRequired,
+    }).isRequired,
+    children: node.isRequired,
+    onEditCell: func,
+    onRenameCell: func,
+    onUpdateCell: func,
+  },
+
+  getInitialState() {
+    return {
+      buttonsOpen: false,
+    }
+  },
+
+  toggleButtons() {
+    const {buttonsOpen} = this.state
+    this.setState({
+      buttonsOpen: !buttonsOpen,
+    })
+  },
+
+  renderButtons() {
+    if (this.state.buttonsOpen) {
+      return (
+        <div style={{width: "50%", float: "right"}}>
+          <button>foo</button>
+          <button>bar</button>
+          <button onClick={this.toggleButtons}>&middot;&middot;&middot;</button>
+        </div>
+      )
+    }
+    return (
+      <div style={{width: "50%", float: "right"}}>
+        <button onClick={this.toggleButtons}>&middot;&middot;&middot;</button>
+      </div>
+    )
+  },
+
+  render() {
+    const {
+      cell,
+      cell: {
+        x,
+        y,
+        name,
+        isEditing,
+      },
+      onEditCell,
+      onRenameCell,
+      onUpdateCell,
+      children,
+    } = this.props
+
+    const isEditable = !!(onEditCell || onRenameCell || onUpdateCell)
+
+    let nameOrField
+    if (isEditing && isEditable) {
+      nameOrField = (
+        <input
+          className="form-control input-sm dash-graph--name-edit"
+          type="text"
+          value={name}
+          autoFocus={true}
+          onChange={onRenameCell(x, y)}
+          onBlur={onUpdateCell(cell)}
+          onKeyUp={(evt) => {
+            if (evt.key === 'Enter') {
+              onUpdateCell(cell)()
+            }
+          }}
+        />
+      )
+    } else {
+      nameOrField = (<span className="dash-graph--name">{name}</span>)
+    }
+
+    let onClickHandler
+    if (isEditable) {
+      onClickHandler = onEditCell
+    } else {
+      onClickHandler = () => {
+        // no-op
+      }
+    }
+
+    return (
+      <div>
+        <div>
+          <h2 style={{width: "50%"}} className="dash-graph--heading" onClick={onClickHandler(x, y, isEditing)}>{nameOrField}</h2>
+          {this.renderButtons()}
+        </div>
+        <div className="dash-graph--container">
+          {children}
+        </div>
+      </div>
+    )
+  },
+})
 
 export default NameableGraph;
