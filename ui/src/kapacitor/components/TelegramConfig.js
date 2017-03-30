d32f0136edbe6d04d29dc9c82ffa143f4e0bd80f
Luke Morris <lukevmorris@gmail.com>
2017-03-30 14:26:00 -0700

    Extract QuestionMarkComponent to its own file

--- ui/src/kapacitor/components/TelegramConfig.js
+++ ui/src/kapacitor/components/TelegramConfig.js
@@ -1,6 +1,5 @@
 import React, {PropTypes} from 'react'
-import ReactTooltip from 'react-tooltip'
-
+import QuestionMarkTooltip from 'src/shared/components/QuestionMarkTooltip'
 const {
   bool,
   func,
@@ -144,24 +143,5 @@ const TelegramConfig = React.createClass({
     )
   },
 })
-
-const QuestionMarkTooltip = ({tipID, tipContent}) => (
-  <div style={{display: "inline-block"}}>
-    <div data-for={`${tipID}-tooltip`} data-tip={tipContent} style={{margin: "0 5px"}}>?</div>
-    <ReactTooltip
-      id={`${tipID}-tooltip`}
-      effect="solid"
-      html={true}
-      offset={{top: 2}}
-      place="bottom"
-      class="influx-tooltip__hover place-bottom"
-    />
-  </div>
-)
-
-QuestionMarkTooltip.propTypes = {
-  tipID: string.isRequired,
-  tipContent: string.isRequired,
-}
 
 export default TelegramConfig
