37dc759d8f599746f1236ea2764ddab6d69b7d41
Chris Goller <goller@gmail.com>
2017-04-02 18:16:08 -0500

    Update /chronograf/v1/ping to simply return 204

--- server/ping.go
+++ server/ping.go
@@ -2,11 +2,7 @@ package server
 
 import "net/http"
 
-// Ping responds with HTTP status 204 (no content) and if auth is available
-// refreshes the token within the cookie.
+// Ping responds with HTTP status 204 (no content)
 func (h *Service) Ping(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
-	if h.UseAuth {
-		// TODO: refresh token
-	}
 }
