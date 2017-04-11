35dfcccbea8f6385a049f54065c0cffc7dbc0b4d
Chris Goller <goller@gmail.com>
2017-04-11 10:24:46 -0500

    Remove unintended console.log from k8s

--- ui/src/kubernetes/containers/KubernetesPage.js
+++ ui/src/kubernetes/containers/KubernetesPage.js
@@ -37,7 +37,6 @@ export const KubernetesPage = React.createClass({
 
   componentDidMount() {
     fetchLayouts().then(({data: {layouts}}) => {
-      console.log(layouts)
       const kubernetesLayouts = layouts.filter((l) => l.app === 'kubernetes')
       this.setState({layouts: kubernetesLayouts})
     })
