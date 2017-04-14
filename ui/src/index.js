9bf488f83b77db7ae1acfb9cc843b66828c5d4d2
Hunter Trujillo <cryptoquick@gmail.com>
2017-04-14 10:55:14 -0600

    Dump localStorage state on any error. This seems to help... A lot!

--- ui/src/index.js
+++ ui/src/index.js
@@ -53,6 +53,12 @@ window.addEventListener('keyup', (event) => {
   }
 })
 
+// Dump localStorage state on any error
+console.error = (error) => {
+  console.warn(error)
+  localStorage.removeItem('state')
+};
+
 const Root = React.createClass({
   getInitialState() {
     return {
