dca4a1bafc373e9e2bca8d396780d4f91d05ed4d
Nathan Haugo <nhaugo@gmail.com>
2017-09-11 16:04:43 -0500

    Remove broken tests after changing group by time logic

--- influx/templates_test.go
+++ influx/templates_test.go
@@ -124,76 +124,6 @@ func TestTemplateReplace(t *testing.T) {
 			},
 			want: `SELECT $field FROM "cpu"`,
 		},
-		{
-			name:  "auto group by",
-			query: `SELECT mean(usage_idle) from "cpu" where time > now() - 4320h group by :interval:`,
-			vars: chronograf.TemplateVars{
-				&chronograf.GroupByVar{
-					Var:               ":interval:",
-					Duration:          180 * 24 * time.Hour,
-					Resolution:        1000,
-					ReportingInterval: 10 * time.Second,
-				},
-			},
-			want: `SELECT mean(usage_idle) from "cpu" where time > now() - 4320h group by time(1555s)`,
-		},
-		{
-			name:  "auto group by without duration",
-			query: `SELECT mean(usage_idle) from "cpu" WHERE time > now() - 4320h group by :interval:`,
-			vars: chronograf.TemplateVars{
-				&chronograf.GroupByVar{
-					Var:               ":interval:",
-					Duration:          0 * time.Minute,
-					Resolution:        1000,
-					ReportingInterval: 10 * time.Second,
-				},
-			},
-			want: `SELECT mean(usage_idle) from "cpu" WHERE time > now() - 4320h group by time(1555s)`,
-		},
-		{
-			name:  "auto group by with :dashboardTime:",
-			query: `SELECT mean(usage_idle) from "cpu" WHERE time > :dashboardTime: group by :interval:`,
-			vars: chronograf.TemplateVars{
-				&chronograf.GroupByVar{
-					Var:               ":interval:",
-					Duration:          0 * time.Minute,
-					Resolution:        1000,
-					ReportingInterval: 10 * time.Second,
-				},
-				&chronograf.BasicTemplateVar{
-					Var: ":dashboardTime:",
-					Values: []chronograf.BasicTemplateValue{
-						{
-							Type:  "constant",
-							Value: "now() - 4320h",
-						},
-					},
-				},
-			},
-			want: `SELECT mean(usage_idle) from "cpu" WHERE time > now() - 4320h group by time(1555s)`,
-		},
-		{
-			name:  "auto group by failing condition",
-			query: `SELECT mean(usage_idle) FROM "cpu" WHERE time > :dashboardTime: GROUP BY :interval:`,
-			vars: []chronograf.TemplateVariable{
-				&chronograf.GroupByVar{
-					Var:               ":interval:",
-					Resolution:        115,
-					ReportingInterval: 10 * time.Second,
-				},
-				chronograf.BasicTemplateVar{
-					Var: ":dashboardTime:",
-					Values: []chronograf.BasicTemplateValue{
-						{
-							Value:    "now() - 1h",
-							Type:     "constant",
-							Selected: true,
-						},
-					},
-				},
-			},
-			want: `SELECT mean(usage_idle) FROM "cpu" WHERE time > now() - 1h GROUP BY time(3s)`,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -258,40 +188,5 @@ func Test_TemplateVarsUnmarshalling(t *testing.T) {
 
 	if !reflect.DeepEqual(tvars[1].(chronograf.BasicTemplateVar), expected[1].(chronograf.BasicTemplateVar)) {
 		t.Errorf("UnmarshalJSON() = \n%#v\n want \n%#v\n", tvars[1].(chronograf.BasicTemplateVar), expected[1].(chronograf.BasicTemplateVar))
-	}
-}
-
-func TestGroupByVarString(t *testing.T) {
-	tests := []struct {
-		name string
-		tvar *chronograf.GroupByVar
-		want string
-	}{
-		{
-			name: "String() calculates the GROUP BY interval",
-			tvar: &chronograf.GroupByVar{
-				Resolution:        700,
-				ReportingInterval: 10 * time.Second,
-				Duration:          24 * time.Hour,
-			},
-			want: "time(12s)",
-		},
-		{
-			name: "String() outputs a minimum of 1s intervals",
-			tvar: &chronograf.GroupByVar{
-				Resolution:        100000,
-				ReportingInterval: 10 * time.Second,
-				Duration:          time.Hour,
-			},
-			want: "time(1s)",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.tvar.String()
-			if got != tt.want {
-				t.Errorf("TestGroupByVarString %s =\n%s\nwant\n%s", tt.name, got, tt.want)
-			}
-		})
 	}
 }
