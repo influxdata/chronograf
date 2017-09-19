5c23ad85aa974ebeb1ce00cd77bbe69030dc5f7d
Jared Scheib <jared.scheib@gmail.com>
2017-09-19 16:05:36 -0700

    Test marshaling of Chronograf user with multitenancy fields

--- bolt/internal/internal_test.go
+++ bolt/internal/internal_test.go
@@ -340,3 +340,30 @@ func Test_MarshalDashboard_WithEmptyLegacyBounds(t *testing.T) {
 		t.Fatalf("Dashboard protobuf copy error: diff follows:\n%s", cmp.Diff(expected, actual))
 	}
 }
+
+func Test_MarshalUser(t *testing.T) {
+	user := chronograf.User{
+		AuthScheme:   "OAuth2",
+		Provider:     "GitHub",
+		Organization: "InfluxData",
+		Email:        "doc@mcfly.time",
+		Name:         "Marty McFly",
+	}
+
+	expected := chronograf.User{
+		AuthScheme:   "OAuth2",
+		Provider:     "GitHub",
+		Organization: "InfluxData",
+		Email:        "doc@mcfly.time",
+		Name:         "Marty McFly",
+	}
+
+	var actual chronograf.User
+	if buf, err := internal.MarshalUser(&user); err != nil {
+		t.Fatal("Error marshaling user: err", err)
+	} else if err := internal.UnmarshalUser(buf, &actual); err != nil {
+		t.Fatal("Error unmarshaling user: err:", err)
+	} else if !cmp.Equal(expected, actual) {
+		t.Fatalf("User protobuf copy error: diff follows:\n%s", cmp.Diff(expected, actual))
+	}
+}
