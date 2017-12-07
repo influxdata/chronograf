5e71391a83d50b7321cd134ee22129450de93c1c
Hunter Trujillo <cryptoquick@gmail.com>
2017-12-07 15:17:06 -0700

    Refactor HSON to JS. Add checkJs option to tsconfig.

--- ui/src/kapacitor/components/KapacitorRule.js
+++ ui/src/kapacitor/components/KapacitorRule.js
@@ -8,7 +8,7 @@ import FancyScrollbar from 'shared/components/FancyScrollbar'
 
 import {createRule, editRule} from 'src/kapacitor/apis'
 import buildInfluxQLQuery from 'utils/influxql'
-import timeRanges from 'hson!shared/data/timeRanges.hson'
+import timeRanges from 'shared/data/timeRanges'
 
 class KapacitorRule extends Component {
   constructor(props) {
