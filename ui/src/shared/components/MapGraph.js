9d269b6c0787808d3ba4c8364a875aa7ee2b0088
Iris Scholten <irisscholten@Iriss-MacBook-Pro.local>
2018-01-24 19:47:18 -0800

    do the things

--- ui/src/shared/components/MapGraph.js
+++ ui/src/shared/components/MapGraph.js
@@ -1,5 +1,6 @@
 import React, {PropTypes, Component} from 'react'
 import Mapbox from 'shared/components/Mapbox'
+import _ from 'lodash'
 
 import SingleStat from 'src/shared/components/SingleStat'
 import timeSeriesToDygraph from 'utils/timeSeriesToDygraph'
@@ -10,29 +11,34 @@ class MapGraph extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data_past: {},
-      data_current: {},
+        data_keys:['key1','name2']
+    //   data_past: []
     }
   }
 
   componentWillMount() {
     const {data} = this.props
     // do something to data here
-    const data_past = data
-    const data_current = data
-    this.setState({data_past, data_current})
+    const dataAll= data[0].response.results[0].series
+    // const dataAll = [{"name":"1","columns":["_time","lat","lon"],"tags":{"hwid":"a"},"values":[[1516841493,0,0],[1516841494,10,10],[1516841495,20,20]]},{"name":"2","columns":["_time","lat","lon"],"tags":{"hwid":"b"},"values":[[1516841493,0,0],[1516841494,0,10],[1516841495,0,20]]},{"name":"3","columns":["_time","lat","lon"],"tags":{"hwid":"c"},"values":[[1516841493,0,0],[1516841494,10,0],[1516841495,20,0]]}]
+
+    const data_past=_.reduce(dataAll, (r, v)=>{
+        const vals = v.values
+        const mappedvals= _.map(vals, (arr)=>[arr[1],arr[2]])
+        r.push(mappedvals)
+        return r
+    }, [])
+    this.setState({data_past})
   }
 
   render() {
-    const {data_past, data_current} = this.state
-    console.log(this.props.data)
+    const {data_past} = this.state
     return (
       <Mapbox
         lat={0}
         lng={0}
         zoom={1}
         data_past={data_past}
-        data_current={data_current}
       />
     )
   }
--- ui/src/shared/components/Mapbox.js
+++ ui/src/shared/components/Mapbox.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-magic-numbers */
 import React, {Component, PropTypes} from 'react'
+import _ from 'lodash'
+import uuid from 'node-uuid'
 
 import mapboxgl from 'mapbox-gl'
 
@@ -18,6 +20,7 @@ class Mapbox extends Component {
 
   componentDidMount() {
     const {lng, lat, zoom} = this.state
+    const {data_past} = this.props
 
     const map = new mapboxgl.Map({
       container: this.mapContainer,
@@ -37,8 +40,9 @@ class Mapbox extends Component {
     })
 
     map.on('load', function() {
+      _.forEach(data_past,(linepts)=>{
       map.addLayer({
-        id: 'route',
+        id: uuid.v4(),
         type: 'line',
         source: {
           type: 'geojson',
@@ -47,29 +51,7 @@ class Mapbox extends Component {
             properties: {},
             geometry: {
               type: 'LineString',
-              coordinates: [
-                [-122.48369693756104, 37.83381888486939],
-                [-122.48348236083984, 37.83317489144141],
-                [-122.48339653015138, 37.83270036637107],
-                [-122.48356819152832, 37.832056363179625],
-                [-122.48404026031496, 37.83114119107971],
-                [-122.48404026031496, 37.83049717427869],
-                [-122.48348236083984, 37.829920943955045],
-                [-122.48356819152832, 37.82954808664175],
-                [-122.48507022857666, 37.82944639795659],
-                [-122.48610019683838, 37.82880236636284],
-                [-122.48695850372314, 37.82931081282506],
-                [-122.48700141906738, 37.83080223556934],
-                [-122.48751640319824, 37.83168351665737],
-                [-122.48803138732912, 37.832158048267786],
-                [-122.48888969421387, 37.83297152392784],
-                [-122.48987674713133, 37.83263257682617],
-                [-122.49043464660643, 37.832937629287755],
-                [-122.49125003814696, 37.832429207817725],
-                [-122.49163627624512, 37.832564787218985],
-                [-122.49223709106445, 37.83337825839438],
-                [-122.49378204345702, 37.83368330777276],
-              ],
+              coordinates: linepts,
             },
           },
         },
@@ -82,48 +64,49 @@ class Mapbox extends Component {
           'line-width': 8,
         },
       })
+    })
 
-      map.addLayer({
-        id: 'points',
-        type: 'symbol',
-        source: {
-          type: 'geojson',
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-77.03238901390978, 38.913188059745586],
-                },
-                properties: {
-                  title: 'Mapbox DC',
-                  icon: 'monument',
-                },
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [-122.414, 37.776],
-                },
-                properties: {
-                  title: 'Mapbox SF',
-                  icon: 'harbor',
-                },
-              },
-            ],
-          },
-        },
-        layout: {
-          'icon-image': '{icon}-15',
-          'text-field': '{title}',
-          'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-          'text-offset': [0, 0.6],
-          'text-anchor': 'top',
-        },
-      })
+      // map.addLayer({
+      //   id: 'points',
+      //   type: 'symbol',
+      //   source: {
+      //     type: 'geojson',
+      //     data: {
+      //       type: 'FeatureCollection',
+      //       features: [
+      //         {
+      //           type: 'Feature',
+      //           geometry: {
+      //             type: 'Point',
+      //             coordinates: [-77.03238901390978, 38.913188059745586],
+      //           },
+      //           properties: {
+      //             title: 'Mapbox DC',
+      //             icon: 'monument',
+      //           },
+      //         },
+      //         {
+      //           type: 'Feature',
+      //           geometry: {
+      //             type: 'Point',
+      //             coordinates: [-122.414, 37.776],
+      //           },
+      //           properties: {
+      //             title: 'Mapbox SF',
+      //             icon: 'harbor',
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   },
+      //   layout: {
+      //     'icon-image': '{icon}-15',
+      //     'text-field': '{title}',
+      //     'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+      //     'text-offset': [0, 0.6],
+      //     'text-anchor': 'top',
+      //   },
+      // })
     })
   }
 
