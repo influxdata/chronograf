8693b420d59234b7ae64ea22ca4379193e67359a
Luke Morris <lukevmorris@gmail.com>
2018-02-28 15:33:56 -0800

    :interval: represents a duration now

--- influx/query.go
+++ influx/query.go
@@ -64,7 +64,7 @@ func Convert(influxQL string) (chronograf.QueryConfig, error) {
 	itsDashboardTime := false
 	intervalTime := false
 	if strings.Contains(influxQL, ":interval:") {
-		influxQL = strings.Replace(influxQL, ":interval:", "time(1234s)", 1)
+		influxQL = strings.Replace(influxQL, ":interval:", "8675309ns", 1)
 		intervalTime = true
 	}
 
@@ -83,7 +83,7 @@ func Convert(influxQL string) (chronograf.QueryConfig, error) {
 	}
 
 	if intervalTime {
-		influxQL = strings.Replace(influxQL, "time(1234s)", ":interval:", 1)
+		influxQL = strings.Replace(influxQL, "8675309ns", ":interval:", 1)
 	}
 
 	raw := chronograf.QueryConfig{
