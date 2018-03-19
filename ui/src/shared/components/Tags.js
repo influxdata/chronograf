dfebea7ec515bd040af9860c814e52e28b007dda
Alex P <thealexpaxton@gmail.com>
2018-03-19 11:28:58 -0700

    Fix positioning of input tags with delete buttons

--- ui/src/shared/components/Tags.js
+++ ui/src/shared/components/Tags.js
@@ -28,17 +28,16 @@ class Tag extends Component {
   render() {
     const {item} = this.props
     return (
-      <span key={item} className="input-tag-item">
+      <span key={item} className="input-tag--item">
         <span>{item.text || item.name || item}</span>
-        {
-          <ConfirmButton
-            icon="remove"
-            size="btn-xs"
-            customClass="btn-xxs"
-            confirmText="Remove user from organization?"
-            confirmAction={this.handleClickDelete(item)}
-          />
-        }
+        <ConfirmButton
+          icon="remove"
+          size="btn-xs"
+          customClass="input-tag--remove"
+          square={true}
+          confirmText="Remove user from organization?"
+          confirmAction={this.handleClickDelete(item)}
+        />
       </span>
     )
   }
