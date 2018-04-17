26f49aba14dfe7a8ce2c3bd2e5449feecd643570
ebb-tide <denizk@gmail.com>
2018-04-17 14:03:09 -0700

    Add mouse movement and clicking to UI test.

--- ui/test/perf/App.test.js
+++ ui/test/perf/App.test.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 const isDebugging = () => {
   const debuggingMode = {
     headless: false,
-    slowMo: 0,
+    slowMo: 250,
     devtools: false,
   }
   return process.env.NODE_ENV === 'debug' ? debuggingMode : {}
@@ -15,23 +15,27 @@ beforeAll(async () => {
   browser = await puppeteer.launch(isDebugging())
   page = await browser.newPage()
 
-  await page.tracing.start({path: 'trace.json'})
-  await page.goto('http://localhost:8888')
-  await page.tracing.stop()
-  page.setViewport({width: 500, height: 2400})
+  await page.goto('http://localhost:8888/sources/2/dashboards/15')
+
+  page.setViewport({width: 1000, height: 2400})
 }, 16000)
 
 describe('on page load ', () => {
   test(
     'Status text is correct',
     async () => {
-      const html = await page.$eval('.page-header__title', e => e.innerHTML)
+      const html = await page.$eval('.dash-graph--name', e => e.innerHTML)
 
-      expect(html).toBe('Status')
-
+      expect(html).toBe('Untitled Cell')
+      await page.tracing.start({path: '../trace3.json'})
+      await page.mouse.move(500, 500, {steps: 10})
+      await page.click('.dash-graph-context--button')
+      await page.click('.dash-graph-context--menu-item')
+      await page.waitForSelector('.overlay-controls')
+      await page.tracing.stop()
       browser.close()
     },
-    16000
+    16001
   )
 })
 
