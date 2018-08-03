2e1e10e40122d15acbcbf01a4a836c411d0c1dbb
Alex P <thealexpaxton@gmail.com>
2018-08-03 16:38:15 -0700

    Test new css import pattern on component in use

--- ui/src/dashboards/components/import_dashboard_mappings/ImportDashboardMappings.tsx
+++ ui/src/dashboards/components/import_dashboard_mappings/ImportDashboardMappings.tsx
@@ -25,6 +25,9 @@ import {
   SourceItemValue,
 } from 'src/types/dashboards'
 
+// Styles
+import 'src/dashboards/components/import_dashboard_mappings/ImportDashboardMappings.scss'
+
 interface Props {
   cells: Cell[]
   source: Source
