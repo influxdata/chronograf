5ad1c68722f7c9bd60aa086c6a5f0bbd1cc5e5ae
Alex P <thealexpaxton@gmail.com>
2018-08-04 10:58:24 -0700

    Implement Panel in InfluxDB Admin
    Didn't all pages because started to require excessive refactoring

--- ui/src/admin/components/QueriesTable.js
+++ ui/src/admin/components/QueriesTable.js
@@ -1,33 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import {Panel, PanelType} from 'src/reusable_ui'
 import QueryRow from 'src/admin/components/QueryRow'
 import {QUERIES_TABLE} from 'src/admin/constants/tableSizing'
 
 const QueriesTable = ({queries, onKillQuery}) => (
-  <div>
-    <div className="panel panel-solid">
-      <div className="panel-body">
-        <table className="table v-center admin-table table-highlight">
-          <thead>
-            <tr>
-              <th style={{width: `${QUERIES_TABLE.colDatabase}px`}}>
-                Database
-              </th>
-              <th>Query</th>
-              <th style={{width: `${QUERIES_TABLE.colRunning}px`}}>Running</th>
-              <th style={{width: `${QUERIES_TABLE.colKillQuery}px`}} />
-            </tr>
-          </thead>
-          <tbody>
-            {queries.map(q => (
-              <QueryRow key={q.id} query={q} onKill={onKillQuery} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
+  <Panel type={PanelType.Solid}>
+    <Panel.Body>
+      <table className="table v-center admin-table table-highlight">
+        <thead>
+          <tr>
+            <th style={{width: `${QUERIES_TABLE.colDatabase}px`}}>Database</th>
+            <th>Query</th>
+            <th style={{width: `${QUERIES_TABLE.colRunning}px`}}>Running</th>
+            <th style={{width: `${QUERIES_TABLE.colKillQuery}px`}} />
+          </tr>
+        </thead>
+        <tbody>
+          {queries.map(q => (
+            <QueryRow key={q.id} query={q} onKill={onKillQuery} />
+          ))}
+        </tbody>
+      </table>
+    </Panel.Body>
+  </Panel>
 )
 
 const {arrayOf, func, shape} = PropTypes
