ebbb6c097ca2f263a3be457b6c852f8250cc9df9
Daniel Campbell <metalwhirlwind@gmail.com>
2018-08-08 18:44:11 -0700

    Completion bar precedes step

--- ui/src/reusable_ui/components/wizard/WizardProgressBar.tsx
+++ ui/src/reusable_ui/components/wizard/WizardProgressBar.tsx
@@ -51,23 +51,22 @@ class WizardProgressBar extends PureComponent<Props> {
       // PROGRESS BAR CONNECTOR
       let connectorStatus = ConnectorState.None
 
-      if (i > 0 && steps[i - 1].stepStatus === StepStatus.Complete) {
+      if (i === currentStepIndex && stepStatus !== StepStatus.Error) {
         connectorStatus = ConnectorState.Some
-
-        if (i === steps.length - 1 || stepStatus === StepStatus.Complete) {
-          connectorStatus = ConnectorState.Full
-        }
+      }
+      if (i === steps.length - 1 || stepStatus === StepStatus.Complete) {
+        connectorStatus = ConnectorState.Full
       }
 
       const connectorEle =
-        i === 0 ? null : (
+        i === steps.length - 1 ? null : (
           <ProgressConnector
             key={`connectorEle${i}`}
             status={connectorStatus}
           />
         )
 
-      return [...acc, connectorEle, stepEle]
+      return [...acc, stepEle, connectorEle]
     }, [])
     return <>{progressBar}</>
   }
