1dfaca74e904aa13045c9e23c8f124bc0d4e990b
Alex P <thealexpaxton@gmail.com>
2018-08-15 09:52:16 -0700

    Write component tests

--- ui/src/reusable_ui/components/dropdowns/MultiSelectDropdown.tsx
+++ ui/src/reusable_ui/components/dropdowns/MultiSelectDropdown.tsx
@@ -173,7 +173,7 @@ class MultiSelectDropdown extends Component<Props, State> {
             autoHeight={true}
             maxHeight={maxMenuHeight}
           >
-            <div className="dropdown--menu">
+            <div className="dropdown--menu" data-test="dropdown-menu">
               {React.Children.map(children, (child: JSX.Element) => {
                 if (this.childTypeIsValid(child)) {
                   if (child.type === DropdownItem) {
