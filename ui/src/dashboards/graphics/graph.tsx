76a04dec9a2ce2040ce34d18fe5cc429c8bb67b1
Alex P <thealexpaxton@gmail.com>
2018-08-24 11:35:47 -0700

    Fix appearance of note svg graphic

--- ui/src/dashboards/graphics/graph.tsx
+++ ui/src/dashboards/graphics/graph.tsx
@@ -27,7 +27,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <polygon
           className="graph-type-selector--graphic-fill graphic-fill-a"
@@ -67,7 +67,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <polygon
           className="graph-type-selector--graphic-fill graphic-fill-a"
@@ -107,7 +107,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <polygon
           className="graph-type-selector--graphic-fill graphic-fill-a"
@@ -147,7 +147,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <path
           className="graph-type-selector--graphic-fill graphic-fill-d"
@@ -187,7 +187,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <g>
           <polygon
@@ -237,7 +237,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <rect
           x="2"
@@ -323,7 +323,7 @@ const GRAPH_SVGS: GraphSVGs = {
         y="0px"
         viewBox="0 0 150 150"
         preserveAspectRatio="none meet"
-        shape-rendering="geometricPrecision"
+        shapeRendering="geometricPrecision"
       >
         <g>
           <path
@@ -530,7 +530,7 @@ const GRAPH_SVGS: GraphSVGs = {
     </div>
   ),
   note: (
-    <div className="viz-type-selector--graphic">
+    <div className="graph-type-selector--graphic">
       <svg
         id="Note"
         x="0px"
@@ -540,11 +540,11 @@ const GRAPH_SVGS: GraphSVGs = {
         viewBox="0 0 150 150"
       >
         <path
-          className="viz-type-selector--graphic-fill graphic-fill-a"
+          className="graph-type-selector--graphic-fill graphic-fill-a"
           d="M61,97.3H37l-4.2,13.6H14l26.7-72.8h16.5l26.9,72.8H65.3L61,97.3z M41.2,83.8h15.7L49,58.5L41.2,83.8z"
         />
         <path
-          className="viz-type-selector--graphic-fill graphic-fill-b"
+          className="graph-type-selector--graphic-fill graphic-fill-b"
           d="M119.1,110.9c-0.6-1.1-1.1-2.7-1.6-4.9c-3.1,3.9-7.4,5.9-13,5.9c-5.1,0-9.4-1.5-13-4.6c-3.6-3.1-5.4-7-5.4-11.6
 	c0-5.9,2.2-10.3,6.5-13.3c4.3-3,10.6-4.5,18.9-4.5h5.2V75c0-5-2.2-7.5-6.5-7.5c-4,0-6,2-6,5.9H87.5c0-5.2,2.2-9.5,6.7-12.7
 	c4.5-3.3,10.1-4.9,17-4.9c6.9,0,12.4,1.7,16.4,5.1c4,3.4,6.1,8,6.2,13.9v24c0.1,5,0.8,8.8,2.3,11.4v0.9H119.1z M108.6,99.9
@@ -552,11 +552,11 @@ const GRAPH_SVGS: GraphSVGs = {
 	C105.7,99.4,107,99.9,108.6,99.9z"
         />
         <path
-          className="viz-type-selector--graphic-line graphic-line-a"
+          className="graph-type-selector--graphic-line graphic-line-a"
           d="M61,97.3H37l-4.2,13.6H14l26.7-72.8h16.5l26.9,72.8H65.3L61,97.3z M41.2,83.8h15.7L49,58.5L41.2,83.8z"
         />
         <path
-          className="viz-type-selector--graphic-line graphic-line-b"
+          className="graph-type-selector--graphic-line graphic-line-b"
           d="M119.1,110.9c-0.6-1.1-1.1-2.7-1.6-4.9c-3.1,3.9-7.4,5.9-13,5.9c-5.1,0-9.4-1.5-13-4.6c-3.6-3.1-5.4-7-5.4-11.6
 	c0-5.9,2.2-10.3,6.5-13.3c4.3-3,10.6-4.5,18.9-4.5h5.2V75c0-5-2.2-7.5-6.5-7.5c-4,0-6,2-6,5.9H87.5c0-5.2,2.2-9.5,6.7-12.7
 	c4.5-3.3,10.1-4.9,17-4.9c6.9,0,12.4,1.7,16.4,5.1c4,3.4,6.1,8,6.2,13.9v24c0.1,5,0.8,8.8,2.3,11.4v0.9H119.1z M108.6,99.9
