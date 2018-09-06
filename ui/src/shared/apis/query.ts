c3794dfb0de8c3c149e4b04fda85b1645c12490c
Christopher Henn <chris@chrishenn.net>
2018-09-06 11:30:52 -0700

    Refactor fetchTimeSeries utility

--- ui/src/shared/apis/query.ts
+++ ui/src/shared/apis/query.ts
@@ -1,4 +1,3 @@
-import _ from 'lodash'
 import {getDeep} from 'src/utils/wrappers'
 import {analyzeQueries} from 'src/shared/apis'
 import {DEFAULT_DURATION_MS} from 'src/shared/constants'
@@ -8,11 +7,7 @@ import {proxy} from 'src/utils/queryUrlGenerator'
 import {Source, Status, Template} from 'src/types'
 import {TimeSeriesResponse, TimeSeriesSeries} from 'src/types/series'
 
-const noop = () => ({
-  type: 'NOOP',
-  payload: {},
-})
-
+// REVIEW: why is this different than the `Query` in src/types?
 interface Query {
   text: string
   database?: string
@@ -21,58 +16,62 @@ interface Query {
   id: string
 }
 
+type EditQueryStatusFunction = (queryID: string, status: Status) => void
+
 export const fetchTimeSeries = async (
   source: Source,
   queries: Query[],
   resolution: number,
   templates: Template[],
   uuid: string,
-  editQueryStatus: () => any = noop
-) => {
-  const timeSeriesPromises = queries.map(async query => {
-    try {
-      const text = await replace(query.text, source, templates, resolution)
-      return handleQueryFetchStatus(
-        {...query, text},
+  editQueryStatus: EditQueryStatusFunction = editQueryStatusNoOp
+) =>
+  Promise.all(
+    queries.map(query =>
+      fetchSingleTimeSeries(
         source,
+        query,
+        resolution,
+        templates,
         uuid,
         editQueryStatus
       )
-    } catch (error) {
-      console.error(error)
-      throw error
-    }
-  })
+    )
+  )
 
-  return Promise.all(timeSeriesPromises)
-}
-
-const handleQueryFetchStatus = async (
-  query: Query,
+const fetchSingleTimeSeries = async (
   source: Source,
+  query: Query,
+  resolution: number,
+  templates: Template[],
   uuid: string,
-  editQueryStatus: () => any
+  editQueryStatus: EditQueryStatusFunction
 ) => {
-  const {database, rp} = query
-  const db = _.get(query, 'db', database)
+  const text = await replace(query.text, source, templates, resolution)
+
+  editQueryStatus(query.id, {loading: true})
 
   try {
-    handleLoading(query, editQueryStatus)
-
-    const payload = {
+    const {data} = await proxy({
       source: source.links.proxy,
-      db,
-      rp,
+      rp: query.rp,
+      query: text,
+      db: query.db || query.database,
       uuid,
-      query: query.text,
+    })
+
+    const warningMessage = extractWarningMessage(data)
+
+    if (warningMessage) {
+      editQueryStatus(query.id, {warn: warningMessage})
+    } else {
+      editQueryStatus(query.id, {success: 'Success!'})
     }
 
-    const {data} = await proxy(payload)
-
-    return handleSuccess(data, query, editQueryStatus)
+    return data
   } catch (error) {
-    console.error(error)
-    handleError(error, query, editQueryStatus)
+    editQueryStatus(query.id, {error: extractErrorMessage(error)})
+
     throw error
   }
 }
@@ -83,78 +82,44 @@ export const replace = async (
   templates: Template[],
   resolution: number
 ): Promise<string> => {
-  try {
-    query = replaceTemplates(query, templates)
-    const durationMs = await duration(query, source)
-    return replaceInterval(query, Math.floor(resolution / 3), durationMs)
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const templateReplacedQuery = replaceTemplates(query, templates)
+  const durationMs = await duration(templateReplacedQuery, source)
+  const replacedQuery = replaceInterval(
+    query,
+    Math.floor(resolution / 3),
+    durationMs
+  )
+
+  return replacedQuery
 }
 
 export const duration = async (
   query: string,
   source: Source
 ): Promise<number> => {
-  try {
-    const analysis = await analyzeQueries(source.links.queries, [{query}])
-    return getDeep<number>(analysis, '0.durationMs', DEFAULT_DURATION_MS)
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const analysis = await analyzeQueries(source.links.queries, [{query}])
+  const queryDuration = getDeep<number>(
+    analysis,
+    '0.durationMs',
+    DEFAULT_DURATION_MS
+  )
+
+  return queryDuration
 }
 
-type EditQueryStatusFunction = (queryID: string, status: Status) => void
+const extractWarningMessage = (data: TimeSeriesResponse): string | null => {
+  const error = getDeep<string>(data, 'results.0.error', null)
+  const series = getDeep<TimeSeriesSeries>(data, 'results.0.series', null)
 
-const handleLoading = (
-  query: Query,
-  editQueryStatus: EditQueryStatusFunction
-): void =>
-  editQueryStatus(query.id, {
-    loading: true,
-  })
-
-const handleSuccess = (
-  data: TimeSeriesResponse,
-  query: Query,
-  editQueryStatus: EditQueryStatusFunction
-): TimeSeriesResponse => {
-  const {results} = data
-  const error = getDeep<string>(results, '0.error', null)
-  const series = getDeep<TimeSeriesSeries>(results, '0.series', null)
-  // 200 from server and no results = warn
-  if (!series && !error) {
-    editQueryStatus(query.id, {
-      warn: 'Your query is syntactically correct but returned no results',
-    })
-    return data
-  }
-
-  // 200 from chrono server but influx returns an "error" = warning
   if (error) {
-    editQueryStatus(query.id, {
-      warn: error,
-    })
-    return data
+    return error
   }
 
-  // 200 from server and results contains data = success
-  editQueryStatus(query.id, {
-    success: 'Success!',
-  })
-  return data
-}
+  if (!series) {
+    return 'Your query is syntactically correct but returned no results'
+  }
 
-const handleError = (
-  error: string,
-  query: Query,
-  editQueryStatus: EditQueryStatusFunction
-): void => {
-  editQueryStatus(query.id, {
-    error: extractErrorMessage(error),
-  })
+  return null
 }
 
 const extractErrorMessage = (errorMessage: string): string => {
@@ -170,3 +135,8 @@ const extractErrorMessage = (errorMessage: string): string => {
 
   return errorMessage
 }
+
+const editQueryStatusNoOp = () => ({
+  type: 'NOOP',
+  payload: {},
+})
