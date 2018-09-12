eed7e7db7b1631d3e3bd9ed9e2701217e92431d6
Alex P <thealexpaxton@gmail.com>
2018-09-12 11:17:36 -0700

    Refactor to be more typescripty and extend types

--- ui/src/shared/components/StaticLegend.tsx
+++ ui/src/shared/components/StaticLegend.tsx
@@ -1,39 +1,52 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+// Libraries
+import React, {Component, MouseEvent} from 'react'
 import _ from 'lodash'
 import uuid from 'uuid'
-import {removeMeasurement} from 'shared/graphs/helpers'
+import classnames from 'classnames'
+
+// Utilities
+import {removeMeasurement} from 'src/shared/graphs/helpers'
+
+// Decorators
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
-const staticLegendItemClassname = (visibilities, i, hoverEnabled) => {
-  if (visibilities.length) {
-    return `${hoverEnabled ? 'static-legend--item' : 'static-legend--single'}${
-      visibilities[i] ? '' : ' disabled'
-    }`
-  }
+// Types
+import {DygraphClass, DygraphSeries} from 'src/types'
 
-  // all series are visible to match expected initial state
-  return 'static-legend--item'
+interface Props {
+  dygraph: DygraphClass
+  dygraphSeries: DygraphSeries
+  height: number
+  onUpdateHeight: (height: number) => void
+}
+
+interface State {
+  visibilities: boolean[]
+  clickStatus: boolean
 }
 
 @ErrorHandling
-class StaticLegend extends Component {
+class StaticLegend extends Component<Props, State> {
+  private staticLegendRef: React.RefObject<HTMLDivElement>
+
   constructor(props) {
     super(props)
+
+    this.staticLegendRef = React.createRef()
+
+    this.state = {
+      visibilities: [],
+      clickStatus: false,
+    }
   }
 
-  state = {
-    visibilities: [],
-    clickStatus: false,
-  }
-
-  componentDidMount = () => {
-    const {height} = this.staticLegendRef.getBoundingClientRect()
+  public componentDidMount = () => {
+    const {height} = this.staticLegendRef.current.getBoundingClientRect()
     this.props.onUpdateHeight(height)
   }
 
-  componentDidUpdate = prevProps => {
-    const {height} = this.staticLegendRef.getBoundingClientRect()
+  public componentDidUpdate = prevProps => {
+    const {height} = this.staticLegendRef.current.getBoundingClientRect()
 
     if (prevProps.height === height) {
       return
@@ -42,17 +55,45 @@ class StaticLegend extends Component {
     this.props.onUpdateHeight(height)
   }
 
-  componentWillUnmount = () => {
+  public componentWillUnmount = () => {
     this.props.onUpdateHeight(0)
   }
 
-  handleClick = i => e => {
+  public render() {
+    const {dygraphSeries} = this.props
+    const {visibilities} = this.state
+    const labels = _.keys(dygraphSeries)
+    const colors = _.map(labels, l => dygraphSeries[l].color)
+
+    const hoverEnabled = labels.length > 1
+
+    return (
+      <div className="static-legend" ref={this.staticLegendRef}>
+        {_.map(labels, (v, i) => (
+          <div
+            className={this.staticLegendItemClassname(
+              visibilities,
+              i,
+              hoverEnabled
+            )}
+            key={uuid.v4()}
+            onMouseDown={this.handleClick(i)}
+          >
+            <span style={{color: colors[i]}}>{removeMeasurement(v)}</span>
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  public handleClick = (i: number) => (e: MouseEvent<HTMLDivElement>): void => {
     const visibilities = this.props.dygraph.visibility()
     const clickStatus = this.state.clickStatus
 
     if (e.shiftKey || e.metaKey) {
       visibilities[i] = !visibilities[i]
       this.props.dygraph.setVisibility(visibilities)
+
       this.setState({visibilities})
       return
     }
@@ -66,48 +107,20 @@ class StaticLegend extends Component {
     newVisibilities[i] = true
 
     this.props.dygraph.setVisibility(newVisibilities)
+
     this.setState({
       visibilities: newVisibilities,
       clickStatus: !prevClickStatus,
     })
   }
 
-  render() {
-    const {dygraphSeries} = this.props
-    const {visibilities} = this.state
-    const labels = _.keys(dygraphSeries)
-    const colors = _.map(labels, l => dygraphSeries[l].color)
-
-    const hoverEnabled = labels.length > 1
-
-    return (
-      <div
-        className="static-legend"
-        ref={s => {
-          this.staticLegendRef = s
-        }}
-      >
-        {_.map(labels, (v, i) => (
-          <div
-            className={staticLegendItemClassname(visibilities, i, hoverEnabled)}
-            key={uuid.v4()}
-            onMouseDown={this.handleClick(i)}
-          >
-            <span style={{color: colors[i]}}>{removeMeasurement(v)}</span>
-          </div>
-        ))}
-      </div>
-    )
+  private staticLegendItemClassname = (visibilities, i, hoverEnabled) => {
+    return classnames('', {
+      disabled: !visibilities[i],
+      'static-legend--item': hoverEnabled,
+      'static-legend--single': !hoverEnabled,
+    })
   }
-}
-
-const {shape, func, number} = PropTypes
-
-StaticLegend.propTypes = {
-  dygraphSeries: shape({}),
-  dygraph: shape({}),
-  height: number.isRequired,
-  onUpdateHeight: func.isRequired,
 }
 
 export default StaticLegend
