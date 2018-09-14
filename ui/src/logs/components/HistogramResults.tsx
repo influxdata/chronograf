eee0247f337e4e871b064a22a47468e3b9128b61
Delmer Reed <delmer814+1@gmail.com>
2018-09-14 15:53:10 -0400

    Update histogram no results message

--- ui/src/logs/components/HistogramResults.tsx
+++ ui/src/logs/components/HistogramResults.tsx
@@ -9,27 +9,36 @@ interface Props {
 
 class HistogramResults extends PureComponent<Props> {
   public render() {
-    return (
-      <label className="logs-viewer--results-text">
-        {this.isPending ? this.pendingContents : this.completedContents}
-      </label>
-    )
+    return <label className="logs-viewer--results-text">{this.contents}</label>
   }
 
-  private get isPending(): boolean {
-    return this.props.searchStatus !== SearchStatus.Loaded
-  }
-
-  private get pendingContents(): JSX.Element {
-    return <>Updating Histogram...</>
+  private get contents() {
+    switch (this.props.searchStatus) {
+      case SearchStatus.NoResults:
+        return this.noResultsContent
+      case SearchStatus.Loaded:
+        return this.completedContents
+      default:
+        return <>Updating Histogram...</>
+    }
   }
 
   private get completedContents(): JSX.Element {
+    const {count} = this.props
+
+    if (count === 0) {
+      return this.noResultsContent
+    }
+
     return (
       <>
-        Displaying <strong> {this.props.count} Events</strong> in Histogram
+        Displaying <strong> {count} Events</strong> in Histogram
       </>
     )
+  }
+
+  private get noResultsContent(): JSX.Element {
+    return <>No Histogram results. Try adjusting the window.</>
   }
 }
 
