9b695abdd28d6e6d9d2b11d1414f56a0de493980
Chris Goller <goller@gmail.com>
2018-09-19 11:14:44 -0700

    feat(import/grafana): add basic grafana types

---
+++ import/grafana/types.go
@@ -0,0 +1,92 @@
+package grafana
+
+type Dashboard struct {
+	Rows       []Row      `json:"rows"`
+	Templating Templating `json:"templating"`
+	Range      Range      `json:"time"`
+	Title      string     `json:"title"`
+}
+
+type Row struct {
+	Height string  `json:"250px"`
+	Panels []Panel `json:"panels"`
+}
+
+type Panel struct {
+	Interval    string   `json:"interval"`
+	Bars        bool     `json:"bars"`
+	Lines       bool     `json:"lines"`
+	Stepped     bool     `json:"steppedLines"`
+	Stacked     bool     `json:"stack"`
+	Span        int      `json:"span"`
+	SpaceLength int      `json:"spaceLength"`
+	Title       string   `json:"title"`
+	Type        string   `json:"graph"`
+	X           XAxis    `json:"xaxis"`
+	Ys          []YAxis  `json:"yaxes"`
+	Targets     []Target `json:"targets"`
+}
+
+type Target struct {
+	Selects     [][]Select `json:"select,omitempty"`
+	RP          string     `json:"policy,omitempty"`
+	Measurement string     `json:"measurement,omitempty"`
+	Tags        []Tag      `json:"tags,omitempty"`
+	GroupBys    []GroupBy  `json:"groupBy,omitempty"`
+
+	Alias      string `json:"alias,omitempty"`
+	SourceType string `json:"dsType,omitempty"`
+	RawQuery   bool   `json:"rawQuery"`
+	Query      string `json:"query"`
+}
+
+func (t *Target) InfluxQL() string {
+	if t.RawQuery {
+		return t.Query
+	}
+
+	query := "SELECT "
+
+	return query
+}
+
+type GroupBy struct {
+	Params []string `json:"params,omitempty"`
+	Type   string   `json:"type,omitempty"`
+}
+
+type Select struct {
+	Params []string `json:"params,omitempty"`
+	Type   string   `json:"type,omitempty"`
+}
+
+type Tag struct {
+	Key      string `json:"key,omitempty"`
+	Operator string `json:"operator,omitempty"`
+	Value    string `json:"value,omitempty"`
+}
+
+type XAxis struct {
+}
+
+type YAxis struct {
+	Format  string `json:"format"`
+	Label   string `json:"label"`
+	LogBase int    `json:"logBase"`
+	Max     int    `json:"max"`
+	Min     int    `json:"min"`
+}
+
+type Templating struct {
+	Templates []Template `json:"list"`
+}
+
+type Template struct {
+	Query string `json:"query"`
+	Type  string `json:"type"`
+}
+
+type Range struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
