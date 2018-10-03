a8481e4b530266ef939f5adfe32c964ec4656ce2
Alirie Gray <alirie.gray@gmail.com>
2018-10-03 15:11:49 -0700

    Remove old flux services from dashboard export

--- ui/src/dashboards/utils/export.ts
+++ ui/src/dashboards/utils/export.ts
@@ -1,22 +1,8 @@
 import _ from 'lodash'
 
-import {Source, Dashboard, Service, Cell} from 'src/types'
+import {Source, Dashboard, Cell} from 'src/types'
 import {CellQuery} from 'src/types/dashboards'
 import {DashboardFile} from 'src/types/dashboards'
-
-export const isFluxService = (link: string) => link.includes('service')
-
-export const mapServicesForDownload = (originalServices: Service[]) =>
-  _.reduce(
-    originalServices,
-    (acc, service) => {
-      const {name, id, links, sourceID} = service
-      const link = _.get(links, 'self', '')
-      acc[id] = {name, link, sourceID}
-      return acc
-    },
-    {}
-  )
 
 export const mapSourcesForDownload = (originalSources: Source[]) =>
   _.reduce(
@@ -30,28 +16,11 @@ export const mapSourcesForDownload = (originalSources: Source[]) =>
     {}
   )
 
-export const matchSourceLink = (cellQuery: CellQuery) => {
-  if (!isFluxService(cellQuery.source)) {
-    return cellQuery.source
-  }
-
-  return null
-}
-
-export const matchServiceLink = (cellQuery: CellQuery) => {
-  if (isFluxService(cellQuery.source)) {
-    return cellQuery.source
-  }
-
-  return null
-}
-
 export const mapQueriesToSources = (queries: CellQuery[]) => {
   return queries.map(query => {
     return {
       ...query,
-      source: matchSourceLink(query),
-      service: matchServiceLink(query),
+      source: query.source,
     }
   })
 }
@@ -69,13 +38,11 @@ export const mapCellsToSources = (cells: Cell[]) => {
 
 export const mapDashboardForDownload = (
   originalSources: Source[],
-  originalServices: Service[],
   originalDashboard: Dashboard,
   chronografVersion: string
 ): DashboardFile => {
   const sources = mapSourcesForDownload(originalSources)
-  const services = mapServicesForDownload(originalServices)
-  const meta = {chronografVersion, sources, services}
+  const meta = {chronografVersion, sources}
 
   const dashboard = {
     ...originalDashboard,
