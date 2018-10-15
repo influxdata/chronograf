3bac0829964a452780761e7acd5e030d0157578e
Christopher Henn <chris@chrishenn.net>
2018-10-15 10:01:44 -0700

    Move autocomplete constant to autocomplete module

--- ui/src/flux/constants/index.ts
+++ ui/src/flux/constants/index.ts
@@ -1,9 +1,8 @@
 import {ast} from 'src/flux/constants/ast'
-import * as editor from 'src/flux/constants/editor'
 import * as argTypes from 'src/flux/constants/argumentTypes'
 import * as vis from 'src/flux/constants/vis'
 import * as explorer from 'src/flux/constants/explorer'
 
 const MAX_RESPONSE_BYTES = 1e7 // 10 MB
 
-export {ast, argTypes, editor, vis, explorer, MAX_RESPONSE_BYTES}
+export {ast, argTypes, vis, explorer, MAX_RESPONSE_BYTES}
