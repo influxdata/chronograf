e73d4152eb7adaf54e0d63786577cb1371e58654
Delmer Reed <delmer814+1@gmail.com>
2018-10-31 18:38:33 -0400

    Add dynamic expanded message container

---
+++ ui/src/logs/components/expandable_message/ExpandedContainer.tsx
@@ -0,0 +1,151 @@
+// Libraries
+import React, {Component, MouseEvent, CSSProperties} from 'react'
+
+// Components
+import {ClickOutside} from 'src/shared/components/ClickOutside'
+import FancyScrollbar from 'src/shared/components/FancyScrollbar'
+
+// Types
+import {NotificationAction} from 'src/types'
+
+// Decorators
+import {ErrorHandling} from 'src/shared/decorators/errors'
+
+interface Props {
+  notify: NotificationAction
+  onClose: () => void
+  maxWidth: number
+  maxHeight: number
+  minWidth: number
+  minLeft: number
+  width: number
+  left: number
+  top: number
+  padding: number
+  children: JSX.Element
+}
+
+interface State {
+  scrollTop: number
+}
+
+const MIN_LEFT = 120
+
+@ErrorHandling
+export class ExpandedContainer extends Component<Props, State> {
+  private initialVisibility: boolean
+
+  constructor(props: Props) {
+    super(props)
+
+    this.initialVisibility = this.isVisible
+    this.state = {scrollTop: 0}
+  }
+
+  public componentDidMount() {
+    if (!this.initialVisibility) {
+      this.props.onClose()
+    }
+  }
+
+  public render() {
+    return (
+      <ClickOutside onClickOutside={this.handleClickOutside}>
+        {this.renderMessage({
+          top: this.top,
+          left: this.maxLeft,
+          width: this.visibleWidth,
+          padding: this.props.padding,
+          maxHeight: this.maxHeight,
+        })}
+      </ClickOutside>
+    )
+  }
+
+  private renderMessage(style: CSSProperties): JSX.Element {
+    if (style.width < this.props.minWidth) {
+      return this.props.children
+    }
+
+    return (
+      <div className="expanded--message message-wrap" style={style}>
+        {this.closeExpansionButton}
+        <FancyScrollbar
+          setScrollTop={this.handleScrollbarScroll}
+          scrollTop={this.state.scrollTop}
+          autoHeight={true}
+          maxHeight={this.props.maxHeight}
+        >
+          {this.props.children}
+        </FancyScrollbar>
+      </div>
+    )
+  }
+
+  private get maxHeight(): number {
+    return this.props.maxHeight + 2 * this.props.padding
+  }
+
+  private get top(): number {
+    return this.props.top - this.props.padding
+  }
+
+  private handleClickDismiss = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation()
+
+    this.props.onClose()
+  }
+
+  private handleScrollbarScroll = (e: MouseEvent<HTMLElement>): void => {
+    e.stopPropagation()
+    e.preventDefault()
+    const {scrollTop} = e.currentTarget
+
+    this.setState({scrollTop})
+  }
+
+  private handleClickOutside = () => {
+    this.props.onClose()
+  }
+
+  private get isVisible(): boolean {
+    return this.visibleWidth > this.props.minWidth
+  }
+
+  private get maxLeft(): number {
+    return Math.max(MIN_LEFT, this.props.left - this.props.padding)
+  }
+
+  private get visibleWidth(): number {
+    return Math.min(
+      this.remainingWidth,
+      this.maxViewableWidth,
+      this.props.width
+    )
+  }
+
+  private get remainingWidth(): number {
+    if (this.props.left > MIN_LEFT) {
+      return this.props.maxWidth - this.hiddenWidth
+    } else {
+      return this.props.width + this.hiddenWidth
+    }
+  }
+
+  private get hiddenWidth(): number {
+    return this.props.left - MIN_LEFT
+  }
+
+  // Table space with room for scrollbar
+  private get maxViewableWidth(): number {
+    return this.props.maxWidth - this.props.padding
+  }
+
+  private get closeExpansionButton(): JSX.Element {
+    return (
+      <button className="expanded--dismiss" onClick={this.handleClickDismiss} />
+    )
+  }
+}
+
+export default ExpandedContainer
