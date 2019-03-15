d00e73c63b40b80ccabdafc492e4cc23842db1e0
Palak Bhojani <palak@influxdata.com>
2019-03-15 09:47:38 -0700

    Fix dashboard to display for another user

--- ui/src/worker/jobs/proxy.ts
+++ ui/src/worker/jobs/proxy.ts
@@ -1,26 +1,19 @@
+import axios from 'axios'
+
 const proxy = async msg => {
   const {
     payload: {url, query, rp, db, uuid},
   } = msg
 
-  const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify({query, rp, db, uuid}),
-  })
+  const body = {url, query, rp, db, uuid}
+  try {
+    const {data} = await axios.post(url, body)
 
-  const data = await response.json()
-
-  if (!response.ok) {
-    let message = 'proxy request failed'
-
-    if (data && data.message) {
-      message = data.message
-    }
-
-    return Promise.reject(message)
+    return {data}
+  } catch (e) {
+    console.error(e)
+    return Promise.reject(e.message)
   }
-
-  return {data}
 }
 
 export default proxy
