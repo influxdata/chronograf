234592dbe5df61c8e82b70c22cd0fa20cc667111
Andrew Watkins <watts@influxdb.com>
2019-07-03 10:54:34 -0700

    feat: add time zone selector to data explorer (#5222)
    * feat(de): add time zone toggle
    
    * chore: christmas time
    
    * feat(ui/tables): add time zones to tables
    
    * fix: prop declaration

--- ui/src/data_explorer/containers/DataExplorer.tsx
+++ ui/src/data_explorer/containers/DataExplorer.tsx
@@ -1,7 +1,6 @@
 // Libraries
 import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
 import {withRouter, InjectedRouter, WithRouterProps} from 'react-router'
 import {Location} from 'history'
 import qs from 'qs'
@@ -36,6 +35,7 @@ import {
 import {writeLineProtocolAsync} from 'src/data_explorer/actions/view/write'
 import {updateSourceLink as updateSourceLinkAction} from 'src/data_explorer/actions/queries'
 import {editQueryStatus as editQueryStatusAction} from 'src/data_explorer/actions/queries'
+import {setTimeZone as setTimeZoneAction} from 'src/shared/actions/app'
 
 import {notify as notifyAction} from 'src/shared/actions/notifications'
 
@@ -60,6 +60,7 @@ import {
   QueryType,
   CellQuery,
   TimeRange,
+  TimeZones,
 } from 'src/types'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 import {Links} from 'src/types/flux'
@@ -88,12 +89,15 @@ interface PassedProps {
   fluxLinks: Links
   notify: (message: Notification) => void
   sourceLink: string
+  onSetTimeZone: typeof setTimeZoneAction
+  timeZone: TimeZones
 }
 
 interface ConnectedProps {
   queryType: QueryType
   queryDrafts: CellQuery[]
   timeRange: TimeRange
+  timeZone: TimeZones
   draftScript: string
   script: string
   onUpdateQueryDrafts: (queryDrafts: CellQuery[]) => void
@@ -157,13 +161,15 @@ export class DataExplorer extends PureComponent<Props, State> {
   public render() {
     const {
       source,
-      sources,
-      editQueryStatus,
-      queryStatus,
-      fluxLinks,
       notify,
-      updateSourceLink,
+      sources,
+      timeZone,
       timeRange,
+      fluxLinks,
+      queryStatus,
+      editQueryStatus,
+      updateSourceLink,
+      onSetTimeZone,
     } = this.props
 
     const {isStaticLegend, isComponentMounted} = this.state
@@ -193,11 +199,13 @@ export class DataExplorer extends PureComponent<Props, State> {
           >
             {(activeEditorTab, onSetActiveEditorTab) => (
               <DEHeader
+                timeZone={timeZone}
                 timeRange={timeRange}
+                onSetTimeZone={onSetTimeZone}
                 activeEditorTab={activeEditorTab}
                 onOpenWriteData={this.handleOpenWriteData}
-                toggleSendToDashboard={this.toggleSendToDashboard}
                 onSetActiveEditorTab={onSetActiveEditorTab}
+                toggleSendToDashboard={this.toggleSendToDashboard}
               />
             )}
           </TimeMachine>
@@ -450,7 +458,7 @@ const ConnectedDataExplorer = (props: PassedProps & WithRouterProps) => {
 const mstp = state => {
   const {
     app: {
-      persisted: {autoRefresh},
+      persisted: {autoRefresh, timeZone},
     },
     dataExplorer: {timeRange, queryStatus, sourceLink},
     dashboardUI: {dashboards},
@@ -459,6 +467,7 @@ const mstp = state => {
   } = state
 
   return {
+    timeZone,
     fluxLinks: links.flux,
     autoRefresh,
     timeRange,
@@ -469,17 +478,16 @@ const mstp = state => {
   }
 }
 
-const mdtp = dispatch => {
-  return {
-    handleChooseAutoRefresh: bindActionCreators(setAutoRefresh, dispatch),
-    errorThrownAction: bindActionCreators(errorThrown, dispatch),
-    writeLineProtocol: bindActionCreators(writeLineProtocolAsync, dispatch),
-    handleGetDashboards: bindActionCreators(getDashboardsAsync, dispatch),
-    sendDashboardCell: bindActionCreators(sendDashboardCellAsync, dispatch),
-    editQueryStatus: bindActionCreators(editQueryStatusAction, dispatch),
-    notify: bindActionCreators(notifyAction, dispatch),
-    updateSourceLink: bindActionCreators(updateSourceLinkAction, dispatch),
-  }
+const mdtp = {
+  handleChooseAutoRefresh: setAutoRefresh,
+  errorThrownAction: errorThrown,
+  writeLineProtocol: writeLineProtocolAsync,
+  handleGetDashboards: getDashboardsAsync,
+  sendDashboardCell: sendDashboardCellAsync,
+  editQueryStatus: editQueryStatusAction,
+  notify: notifyAction,
+  updateSourceLink: updateSourceLinkAction,
+  onSetTimeZone: setTimeZoneAction,
 }
 
 export default connect(mstp, mdtp)(withRouter(ConnectedDataExplorer))
