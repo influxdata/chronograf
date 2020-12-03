1bd1ab5245ac570a171b245d8a4450fe9809051c
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-12-03 11:24:52 +0100

    fix(ui): repair visual comparison with time variables

--- ui/test/shared/query/helpers.test.js
+++ ui/test/shared/query/helpers.test.js
@@ -47,6 +47,33 @@ describe('Shared.Query.Helpers', () => {
 
       expect(timeRangeType(timeRange)).toBe(RELATIVE_UPPER)
     })
+
+    it('can detect absolute type with variables', () => {
+      const upper = ':upperDashboardTime:'
+      const lower = ':dashboardTime:'
+
+      const timeRange = {lower, upper, format}
+
+      expect(timeRangeType(timeRange)).toBe(ABSOLUTE)
+    })
+
+    it('can detect exclusive relative lower with variable', () => {
+      const lower = ':dashboardTime:'
+      const upper = null
+
+      const timeRange = {lower, upper, format}
+
+      expect(timeRangeType(timeRange)).toBe(RELATIVE_LOWER)
+    })
+
+    it('can detect relative upper with variable', () => {
+      const upper = 'now()'
+      const lower = ':dashboardTime:'
+
+      const timeRange = {lower, upper, format}
+
+      expect(timeRangeType(timeRange)).toBe(RELATIVE_UPPER)
+    })
   })
 
   describe('timeRangeShift', () => {
@@ -102,6 +129,41 @@ describe('Shared.Query.Helpers', () => {
       }
 
       expect(type).toBe(ABSOLUTE)
+      expect(actual).toEqual(expected)
+    })
+
+    it('can calculate the shift for absolute timeRanges with variables', () => {
+      const upper = ':upperDashboardTime:'
+      const lower = ':dashboardTime:'
+      const shift = {quantity: 7, unit: 'd'}
+      const timeRange = {upper, lower}
+
+      const type = timeRangeType(timeRange)
+      const actual = shiftTimeRange(timeRange, shift)
+      const expected = {
+        lower: `${lower} - 7d`,
+        upper: `${upper} - 7d`,
+        type: 'shifted',
+      }
+
+      expect(type).toBe(ABSOLUTE)
+      expect(actual).toEqual(expected)
+    })
+
+    it('can calculate the shift for relative lower timeRanges with variables', () => {
+      const shift = {quantity: 7, unit: 'd'}
+      const lower = ':dashboardTime:'
+      const timeRange = {lower, upper: null}
+
+      const type = timeRangeType(timeRange)
+      const actual = shiftTimeRange(timeRange, shift)
+      const expected = {
+        lower: `${lower} - 7d`,
+        upper: `now() - 7d`,
+        type: 'shifted',
+      }
+
+      expect(type).toBe(RELATIVE_LOWER)
       expect(actual).toEqual(expected)
     })
   })
