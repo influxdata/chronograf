cc81dfd16c6b4b2ae2ec1ac9412db047edd81650
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-12-08 05:21:06 +0100

    feat(ui/flux): improve error handling

--- ui/src/flux/helpers/scriptBuilder.ts
+++ ui/src/flux/helpers/scriptBuilder.ts
@@ -5,10 +5,21 @@ interface Status {
 
 export const parseError = (error): Status => {
   if (error.data) {
-    const s = error.data.slice(0, -5) // There is a 'null\n' at the end of these responses
-    const data = JSON.parse(s)
-    return {type: 'error', text: data.message}
+    if (typeof error.data === 'string') {
+      const s = error.data.slice(0, -5) // There is a 'null\n' at the end of these responses
+      const data = JSON.parse(s)
+      return {type: 'error', text: data.message}
+    } else if (typeof error.data === 'object') {
+      if (error.data.error) {
+        return {type: 'error', text: error.data.error}
+      }
+    }
   }
-
-  return {type: 'error', text: error.message}
+  if (error.message) {
+    return {type: 'error', text: error.message}
+  }
+  if (error.statusText) {
+    return {type: 'error', text: error.statusText}
+  }
+  return {type: 'error', text: error.toString()}
 }
