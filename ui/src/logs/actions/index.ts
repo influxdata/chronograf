590b3097b6caabeb60f219de25f8733ccb4f6be7
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-03-08 23:01:59 +0100

    chore: fix eslint errors

--- ui/src/logs/actions/index.ts
+++ ui/src/logs/actions/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/ban-types */
 import _ from 'lodash'
 import {Dispatch} from 'redux'
 import {ThunkDispatch} from 'redux-thunk'
