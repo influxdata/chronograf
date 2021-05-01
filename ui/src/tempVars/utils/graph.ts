8f885e81d06d35336ba1633d79fdee57fc35ba2b
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-05-01 12:38:07 +0200

    fix(ui): repair default fetcher

--- ui/src/tempVars/utils/graph.ts
+++ ui/src/tempVars/utils/graph.ts
@@ -182,8 +182,6 @@ function isResolved(node: TemplateNode): boolean {
 }
 
 class CachingTemplateQueryFetcher implements TemplateQueryFetcher {
-  private proxyUrl: string
-
   private cache: {
     [proxyUrl: string]: {
       [query: string]: string[]
@@ -209,7 +207,7 @@ class CachingTemplateQueryFetcher implements TemplateQueryFetcher {
     }
     const proxyURL = flux ? source.links.flux : source.links.proxy
     if (!proxyURL) {
-      warn('Flux endpoint is not available!')
+      warn(`${flux ? 'Flux' : 'Proxy'} endpoint is not available!`)
       return []
     }
 
@@ -243,7 +241,7 @@ class CachingTemplateQueryFetcher implements TemplateQueryFetcher {
         }
       }
     } else {
-      const response = await proxy({source: this.proxyUrl, query})
+      const response = await proxy({source: proxyURL, query})
       values = parseMetaQuery(query, response.data)
     }
 
