bbec0ff303a4b81561c82e789d36d6cac0b63ae1
Iszy-Ami <huyiktran@gmail.com>
2021-06-10 15:38:54 +0200

    fix: selecting dynamic dropdown item

--- ui/cypress/integration/cy_label.test.ts
+++ ui/cypress/integration/cy_label.test.ts
@@ -3,8 +3,15 @@ describe.only('Navigate', () => {
 
   it('_start and _stop display', () => {
     cy.clickNav(3, 'Explore')
-    cy.get('[data-test=source-button-selector] > .dropdown > .dropdown--button').click()
-    cy.get(':nth-child(1) > .dropdown-item--children').should('include.text', 'InfluxTest2').click()
+    cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
+    // selecting dynamic dropdown item
+    cy.get('[data-test="dropdown--item"]')
+      .each(($el, index, $list) => {
+        if($el.text() == 'InfluxTest2'){
+          $el.click()}
+      })
+    //cy.get(':nth-child(1) > .dropdown-item--children').should('include.text', 'InfluxTest2').click()
     cy.get('[data-test=source-button-selector] > .radio-buttons').contains('Flux').click({force: true})
     cy.get('[data-test=flux-schema-flux-schema-tree]').click({multiple: true})
     cy.get('[data-test="schema-category-field"]')
