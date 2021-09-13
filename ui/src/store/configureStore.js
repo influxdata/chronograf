ca788671beaf926d559f2fdeabf619a5e18230bb
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-09-13 14:31:12 +0200

    fix(ui): ensure one organization between tabs

--- ui/src/store/configureStore.js
+++ ui/src/store/configureStore.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware, compose} from 'redux'
 import {combineReducers} from 'redux'
 import {routerReducer, routerMiddleware} from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk'
+import {get} from 'lodash'
 
 import errorsMiddleware from 'shared/middleware/errors'
 import {resizeLayout} from 'shared/middleware/resizeLayout'
@@ -36,6 +37,9 @@ const rootReducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const KEY_ORG = 'orgchrono' // local storage key holding active organization
+let currentOrg // active organization, possibly empty or undefined
+
 export default function configureStore(initialState, browserHistory) {
   const routingMiddleware = routerMiddleware(browserHistory)
   const createPersistentStore = composeEnhancers(
@@ -45,12 +49,60 @@ export default function configureStore(initialState, browserHistory) {
       routingMiddleware,
       errorsMiddleware,
       queryStringConfig,
-      resizeLayout
+      resizeLayout,
+      signalizeChangedOrg
     )
   )(createStore)
+
+  // reload whenever current organization is changed from another tab/window
+  try {
+    currentOrg = window.localStorage.getItem(KEY_ORG)
+    window.addEventListener('storage', function (e) {
+      if (e.storageArea !== window.localStorage && e.key !== KEY_ORG) {
+        return
+      }
+      if (e.newValue !== currentOrg) {
+        // reload the page on organization change made from another tab
+        window.location.reload()
+      }
+    })
+  } catch (e) {
+    // ignore window.localStorage unavailability or getItem error
+    console.error(e)
+  }
 
   // https://github.com/elgerlambert/redux-localstorage/issues/42
   // createPersistantStore should ONLY take reducer and initialState
   // any store enhancers must be added to the compose() function.
   return createPersistentStore(rootReducer, initialState)
+}
+
+/**
+ * SignalizeChangedOrg is a redux middleware that
+ * stores current organization ID into localStorage so that
+ * browser windows/tabs can be notified about session change.
+ */
+const signalizeChangedOrg = () => next => action => {
+  next(action)
+
+  try {
+    if (action.type === 'ME_GET_COMPLETED') {
+      const orgId = get(
+        action,
+        ['payload', 'me', 'currentOrganization', 'id'],
+        ''
+      )
+      if (orgId !== currentOrg) {
+        currentOrg = orgId
+        window.localStorage.setItem(KEY_ORG, currentOrg)
+      }
+    }
+    if (action.type === 'AUTH_EXPIRED') {
+      currentOrg = ''
+      window.localStorage.setItem(KEY_ORG, currentOrg)
+    }
+  } catch (e) {
+    // ignore window.localStorage unavailability or setItem errors
+    console.error(e)
+  }
 }
