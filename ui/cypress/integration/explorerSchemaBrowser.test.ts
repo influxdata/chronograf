a710ac4697b11ffe585ac20aae9a1d3d3c1c349d
Iszy-Ami <huyiktran@gmail.com>
2021-09-20 13:50:30 +0200

    test: snapshot testing

--- ui/cypress/integration/explorerSchemaBrowser.test.ts
+++ ui/cypress/integration/explorerSchemaBrowser.test.ts
@@ -24,55 +24,89 @@ describe('Navigate', () => {
       .should('not.exist')
   })
 
+  it('write data manual', () => {
+    cy.clickNav(3, 'Explore')
+    cy.get('[data-test=source-button-selector] > .dropdown > .dropdown--button')
+      .click()
+    cy.get('[data-test=dropdown--item]')
+      .filter(':contains("Influx 2")')
+      .click()
+    cy.getByTitle('Flux').click()
 
+    cy.writeManualData('new_data', 'value=2')
+    cy.writeManualData('new_data', 'value=2.5')
+    cy.writeManualData('new_data', 'value=5.8')
+    cy.writeManualData('new_data', 'value=3.7')
+    cy.writeManualData('new_data', 'value=4.5')
+
+    cy.get('[data-test=threesizer-header-controls] > .button-default').click('left')
+    cy.get('.form--wrapper > :nth-child(1) > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
   
+    cy.get('[data-test="dropdown--item"]')
+      .filter(':contains("_tasks")')
+      .click()
+    cy.get(':nth-child(2) > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
+      .filter(':contains("new_data")').click()
+    cy.get('.form--submit > .button').click()
+    cy.get('.button-primary').click()
+
+    cy.get('[data-test="page-header--center"]').click('right')
+    cy.get('#Table').click()
+
+    cy.get('[data-test="table-graph"]').contains('2.5')
+    cy.get('[data-test="table-graph"]').contains('5.8')
+  })
+
+it('default connetion to influxdb v2', ()=> {
+  cy.clickNav(8, 'Configuration')
+  cy.get('h1')
+  cy.getByTestID('set-conection-panel-body').contains('Influx 2 (Default)')
+  })
+
+  it('add data to explore', () => { 
+    cy.clickNav(3, 'Explore')
+    cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
+    // selecting dynamic dropdown item
+    cy.get('[data-test="dropdown--item"]')
+      .each(($el, index, $list) => {
+        if($el.text() == 'InfluxTest2'){
+          $el.click()
+        }
+      })
   
-    it('default connetion to influxdb v2', ()=> {
-      cy.clickNav(8, 'Configuration')
-      cy.get('h1')
-      cy.getByTestID('set-conection-panel-body').contains('Influx 2 (Default)')
-    })
-  
-    it('add data to explore', () => { 
-      cy.clickNav(3, 'Explore')
-  
-      cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
-        .click()
-      // selecting dynamic dropdown item
-      cy.get('[data-test="dropdown--item"]')
-        .each(($el, index, $list) => {
-          if($el.text() == 'InfluxTest2'){
-            $el.click()}
-    })
-  
-      cy.getByTitle('Flux').click()
-      cy.writeManualData('new_data', 'value=2')
-      cy.writeManualData('new_data', 'value=2.5')
-      cy.writeManualData('new_data', 'value=5.8')
-      cy.writeManualData('new_data', 'value=3.7')
-      cy.writeManualData('new_data', 'value=5')
-      cy.writeManualData('new_data', 'value=2.5')
-      cy.writeManualData('new_data', 'value=5.8')
-      cy.writeManualData('new_data', 'value=3.7')
+    cy.getByTitle('Flux').click()
+    cy.writeManualData('new_data', 'value=2')
+    cy.writeManualData('new_data', 'value=2.5')
+    cy.writeManualData('new_data', 'value=5.8')
+    cy.writeManualData('new_data', 'value=3.7')
+    cy.writeManualData('new_data', 'value=5')
+    cy.writeManualData('new_data', 'value=2.5')
+    cy.writeManualData('new_data', 'value=5.8')
+    cy.writeManualData('new_data', 'value=3.7')
       
-      cy.clickNav(4, 'Dashboards')
-      cy.getByTitle('Create Dashboard').click()
-      cy.getByTestID('rename-dashboard')
-      //.clear()
+    cy.clickNav(4, 'Dashboards')
+    cy.getByTitle('Create Dashboard').click()
+    cy.getByTestID('rename-dashboard')
+    // .clear()
       .type('explorer_test{enter}')
-      cy.getByTestID('add-data-btn').click()
+    cy.getByTestID('add-data-btn').click()
   
-      cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
-        .click()
+    cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
     
       // selecting dynamic dropdown item
-      cy.get('[data-test="dropdown--item"]')
-        .each(($el, index, $list) => {
-          if($el.text() == 'InfluxTest2'){
-            $el.click()}})
+    cy.get('[data-test="dropdown--item"]')
+      .each(($el, index, $list) => {
+        if($el.text() == 'InfluxTest2'){
+          $el.click()
+        }
+      })
     
-      cy.getByTitle('Flux').click()
-      cy.getByTitle('Script Wizard')
+    cy.getByTitle('Flux').click()
+    cy.getByTitle('Script Wizard')
   })
   
   it('create data queries in data explore',() =>{
@@ -133,5 +167,75 @@ describe('Navigate', () => {
       cy.get(':nth-child(1) > .form-control').type('Amos ')
     // edit graph
       cy.getByTitle('Save').click()
+  })
+  it('snapshot',() =>{
+    cy.clickNav(3, 'Explore')
+
+    cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
+    // selecting dynamic dropdown item
+    cy.get('[data-test="dropdown--item"]')
+      .each(($el, index, $list) => {
+        if($el.text() == 'InfluxTest2'){
+          $el.click()}
+    })
+
+    cy.getByTitle('Flux').click()
+    cy.writeManualData('testing_value', 'value=0')
+
+    
+    cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+      .click()
+
+  // selecting dynamic dropdown item
+  cy.clickNav(4, 'Dashboards')
+  cy.getByTitle('Create Dashboard').click()
+  cy.getByTestID('rename-dashboard')
+    .click()
+    .type('explorer_test{enter}')
+  cy.getByTestID('add-data-btn').click()
+
+  cy.get('[data-test=source-button-selector] > .dropdown > [data-test=wizard-bucket-selected]')
+    .click()
+  
+  // selecting dynamic dropdown item
+  cy.get('[data-test="dropdown--item"]')
+    .each(($el, index, $list) => {
+      if($el.text() == 'InfluxTest2'){
+        $el.click()}})
+  
+  cy.getByTitle('Flux').click()
+
+  cy.getByTitle('Script Wizard').click()
+  cy.get('.form--wrapper > :nth-child(1) > .dropdown > [data-test=wizard-bucket-selected]')
+    .click()
+
+  cy.getByTestID('dropdown--item')
+    .filter(':contains("_tasks")')
+    .click()
+  cy.get(':nth-child(2) > .dropdown > [data-test=wizard-bucket-selected]')
+        .click()
+    /*.filter(':contains("testing_value")').click()*/
+  cy.get('[data-test="dropdown--item"]')
+    .each(($el, index, $list) => {
+      if($el.text() == 'testing_value'){
+        $el.click()}})
+  cy.get('.form--submit > .button').click()
+
+  cy. getByTitle('Visualization').click()
+  cy.get('#SingleStat').click()
+  cy.getByTestID('graph-container')
+    .wait(500)
+
+  cy.getByTestID('graph-container')
+    .matchImageSnapshot('singleNumber')
+
+  //edit graph
+  cy.getByTitle('Save').click()
+
+    cy.get('.dash-graph--container')
+    .wait(1500)
+  cy.get('.dash-graph--container')
+    .matchImageSnapshot('dashboardSingleNumber')
   })
 })
--- ui/cypress/support/commands.ts
+++ ui/cypress/support/commands.ts
@@ -1,38 +1,32 @@
 import {addMatchImageSnapshotCommand} from 'cypress-image-snapshot/command'
 
-/* declare namespace Cypress {
-  interface Chainable {
-    click()
-    first()
+declare namespace Cypress {
+  interface Chainable<Subject> {
     /**
      * Custom command to match image snapshots.
      * @example cy.matchImageSnapshot('greeting')
      */
-/*    matchImageSnapshot(snapshotName?: string): void
+    matchImageSnapshot(snapshotName?: string): void;
   }
 }
-*/
-// We set up the settings
-/* addMatchImageSnapshotCommand({
-  customSnapshotsDir: 'src/snapshots',
+
+// Set up the settings
+addMatchImageSnapshotCommand({
+  customSnapshotsDir: '../ui/cypress/snapshots',
   failureThreshold: 0.75, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
-  customDiffConfig: {threshold: 0.75}, // threshold for each pixel
+  customDiffConfig: { threshold: 0.75 }, // threshold for each pixel
   capture: 'viewport', // capture viewport in screenshot
 })
 
-// We also overwrite the command, so it does not take a sceenshot if we run the tests inside the test runner
-Cypress.Commands.overwrite(
-  'matchImageSnapshot',
-  (originalFn, snapshotName, options) => {
-    if (Cypress.env('ALLOW_SCREENSHOT')) {
-      originalFn(snapshotName, options)
-    } else {
-      cy.log(`Screenshot comparison is disabled`)
-    }
+Cypress.Commands.overwrite('matchImageSnapshot', (originalFn, snapshotName, options) => {
+  if (Cypress.env('ALLOW_SCREENSHOT')) {
+    originalFn(snapshotName, options)
+  } else {
+    cy.log(`Screenshot comparison is disabled`)
   }
-)
- */
+})
+
 export const clickNav = (index: number, label: string): Cypress.Chainable => {
   cy.get(`:nth-child(${index}) > .sidebar--square`).click()
   return cy.get('h1.page-header--title').should('have.text', `${label}`)
