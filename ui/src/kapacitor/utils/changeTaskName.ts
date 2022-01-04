2933b1d493c3789c0d86c0282933c7a2f11a59bf
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-01-04 11:44:01 +0100

    feat(ui): implement tickscript name change

--- ui/src/kapacitor/utils/changeTaskName.ts
+++ ui/src/kapacitor/utils/changeTaskName.ts
@@ -1,13 +1,29 @@
+const reNameDeclaration = new RegExp(
+  // eslint-disable-next-line no-control-regex
+  "(?:^|\n)var[ \t]+name[ \t]*=[ \t]*'[^\n]+'\\s*?[\n]"
+)
+
+function escapeName(s: string) {
+  return s.replace(/['\\]/gi, (str: string): string => '\\' + str)
+}
+
 /**
- * Changes or creates a name variable in the supplied tickscript
- * to be a the newName supplied.
+ * Changes or creates a name variable in the supplied tickscript.
  * @param tickscript tickscript
  * @param newName new name
- * @returns modified tickscript
+ * @returns modified tickscript with a new name
  */
 export default function changeTaskName(
   tickscript: string,
   newName: string
 ): string {
-  return `// TODO change var name to ${newName}\n${tickscript}`
+  const match = tickscript.match(reNameDeclaration)
+  if (!match) {
+    return `var name = '${escapeName(newName)}'\n${tickscript}`
+  }
+  let retVal = match.index ? `${tickscript.substring(0, match.index)}\n` : ''
+  retVal += `var name = '${escapeName(newName)}'\n${tickscript.substring(
+    match.index + match[0].length
+  )}`
+  return retVal
 }
