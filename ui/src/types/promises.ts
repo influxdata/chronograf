5a1b3ec08cb2246a2a5198b7d8b925dc9ff419d3
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-01-28 08:55:09 +0100

    feat(fluxWizard): add CancelBox

--- ui/src/types/promises.ts
+++ ui/src/types/promises.ts
@@ -2,3 +2,14 @@ export interface WrappedCancelablePromise<T> {
   promise: Promise<T>
   cancel: () => void
 }
+export interface CancelBox<T> extends WrappedCancelablePromise<T> {
+  id?: string
+}
+
+export class CancellationError extends Error {
+  constructor(...args: any[]) {
+    super(...args)
+
+    this.name = 'CancellationError'
+  }
+}
