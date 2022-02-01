89365d64b1238c738e1d2e26e34944d8fbb6723c
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-02-01 18:53:47 +0100

    feat(fluxWizard): introduce flux script builder

--- ui/src/shared/components/TimeMachine/fluxQueryBuilder/util/generateFlux.ts
+++ ui/src/shared/components/TimeMachine/fluxQueryBuilder/util/generateFlux.ts
@@ -1,5 +1,6 @@
 import {TimeRange} from 'src/types'
-import {BuilderTagsType} from '../types'
+import {BuilderTagsType, QueryBuilderState} from '../types'
+import {AGG_WINDOW_AUTO, FUNCTIONS} from './constants'
 
 export function formatTimeRangeArguments(timeRange: TimeRange): string {
   const start = timeRange.lowerFlux ? timeRange.lowerFlux : timeRange.lower
@@ -15,4 +16,69 @@ export function tagToFlux(tag: BuilderTagsType) {
 }
 export function fluxString(s: string = '') {
   return s.replace(/\\/g, '\\\\').replace(/"/g, '\\"')
+}
+
+export function buildQuery(state: QueryBuilderState): string | undefined {
+  const bucket = state.buckets.selectedBucket
+  if (!bucket) {
+    return
+  }
+  let query = 'from(bucket: "${bucket}'
+  query += '\n  |> range(start: v.timeRangeStart, stop: v.timeRangeStart)'
+  state.tags.forEach(tag => {
+    query += formatTagFluxFunction(tag)
+  })
+
+  const functions = state.aggregation.selectedFunctions
+  if (functions.length === 0) {
+    return query
+  }
+  const period = state.aggregation.period
+  const fillMissing = state.aggregation.fillMissing
+  return functions
+    .map(fn => query + formatAggregationFunction(fn, period, fillMissing))
+    .join('\n\n')
+}
+
+function formatTagFluxFunction(tag: BuilderTagsType) {
+  if (!tag.tagKey) {
+    return ''
+  }
+  if (tag.aggregateFunctionType === 'filter') {
+    if (!tag.tagValues.length) {
+      return ''
+    }
+    return `\n  |> filter(fn: (r) => ${tagToFlux(tag)})`
+  }
+
+  if (tag.aggregateFunctionType === 'group') {
+    if (!tag.tagValues?.length) {
+      return '' // do not group when no values selected
+    }
+    return `\n  |> group(columns: [${tag.tagValues
+      .map(x => `"${fluxString(x)}"`)
+      .join(', ')}])`
+  }
+}
+function formatAggregationFunction(
+  fn: string,
+  period: string,
+  fillValues: boolean
+) {
+  const fnSpec = FUNCTIONS.find(spec => spec.name === fn)
+  if (!fnSpec) {
+    return '\n  |> yield(name: "${fn}")'
+  }
+
+  return `\n  ${fnSpec.flux(
+    formatPeriod(period),
+    fillValues
+  )}\n  |> yield(name: "${fn}")`
+}
+
+const formatPeriod = (period: string): string => {
+  if (period === AGG_WINDOW_AUTO || !period) {
+    return `v.windowPeriod`
+  }
+  return period
 }
