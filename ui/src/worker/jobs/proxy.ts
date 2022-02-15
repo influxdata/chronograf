f870eb772b2264a3c559b0f637612806c8750094
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-02-15 07:58:34 +0100

    chore(ui): remove axios from proxy worker

--- ui/src/worker/jobs/proxy.ts
+++ ui/src/worker/jobs/proxy.ts
@@ -1,24 +1,41 @@
-import axios from 'axios'
-
-const proxy = async msg => {
+interface ProxyMsg {
+  payload: {
+    url: string
+    query: string
+    rp?: string
+    db?: string
+    uuid?: string
+  }
+}
+const proxy = async (msg: ProxyMsg): Promise<{data: any}> => {
   const {
     payload: {url, query, rp, db, uuid},
   } = msg
 
   const body = {url, query, rp, db, uuid}
   try {
-    const {data} = await axios.post(url, body)
-
-    return {data}
-  } catch (e) {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+    })
+    if (response.ok) {
+      return {data: await response.json()}
+    }
+    const data = await response.text()
     console.error(
-      e.message,
-      '; response:',
-      e.response?.data,
-      '; request:',
-      body
+      'failed to POST url:',
+      url,
+      'body:',
+      body,
+      'status:',
+      response.statusText,
+      'response:',
+      data
     )
-    return Promise.reject(e.message)
+    Promise.reject(response.statusText || `error ${response.status}`)
+  } catch (e) {
+    console.error('failed to POST url:', url, 'body:', body, 'error:', e)
+    return Promise.reject(e.message ? e.message : String(e))
   }
 }
 
