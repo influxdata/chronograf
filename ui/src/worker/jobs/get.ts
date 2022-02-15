50bd143c57da8b1164a119ed376a43006669be6e
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-02-15 08:06:48 +0100

    chore(ui): remove axios from get worker

--- ui/src/worker/jobs/get.ts
+++ ui/src/worker/jobs/get.ts
@@ -1,13 +1,30 @@
-import {Message} from 'src/worker/types'
-import axios from 'axios'
+import {Message} from '../types'
 
-const get = async (msg: Message) => {
+const get = async (msg: Message): Promise<any> => {
   const {
     payload: {url},
   } = msg
-
-  const {data} = await axios.get(url)
-  return data
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+    })
+    if (response.ok) {
+      return response.json()
+    }
+    const data = await response.text()
+    console.error(
+      'failed to GET url:',
+      url,
+      'status:',
+      response.statusText,
+      'response:',
+      data
+    )
+    Promise.reject(new Error(response.statusText || `error ${response.status}`))
+  } catch (e) {
+    console.error('failed to GET url:', url, 'error:', e)
+    return Promise.reject(e)
+  }
 }
 
 export default get
