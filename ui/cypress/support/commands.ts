d05129145ea322bb8c8b54b8f565960a139322fa
k3yi0 <definitelynotkeyio@gmail.com>
2022-02-21 17:11:18 +0100

    test(cypress): add cypress env, and commands

--- ui/cypress/support/commands.ts
+++ ui/cypress/support/commands.ts
@@ -27,86 +27,103 @@ Cypress.Commands.overwrite('matchImageSnapshot', (originalFn, snapshotName, opti
   }
 })
 
-export const clickNav = (index: number, label: string): Cypress.Chainable => {
-  cy.get(`:nth-child(${index}) > .sidebar--square`).click()
-  return cy.get('h1.page-header--title').should('have.text', `${label}`)
-}
-
 export const getByTestID = (
   dataTest: string,
   options?: Partial<
     Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
   >
 ): Cypress.Chainable => {
-  return cy.get(`[data-test="${dataTest}"]`, options)
+  return cy.get(`[data-testid="${dataTest}"]`, options)
 }
 
-export const getByTitle = (name: string): Cypress.Chainable => {
-  return cy.get(`[title="${name}"]`)
+// Change enviromental values in cypress.json
+export const createConnection = (url: string = Cypress.env('url')) => {
+  return cy
+    .request(
+      {
+        method: 'POST',
+        url: '/chronograf/v1/sources',
+        body :{
+          url: url,
+          username: Cypress.env('username'),
+          password: Cypress.env('password'),
+          name: Cypress.env('connectionName'),
+        }
+      })
+      .then(() => {
+        wrapConnections()
+      })
 }
 
-function createConnectionV2(url: string,
-  connectionName: string,
-  organization: string,
-  token: string,
-  dbname: string,
-) {
-cy.get('div[title="InfluxDB v2 Auth"]').click()
-cy.get('input[id="Connection URL"]').clear().type(url)
-cy.get('input[id="Connection Name"]').clear().type(connectionName)
-cy.get('input[id="Organization"]').clear().type(organization)
-cy.get('input[id="Token"]').clear().type(token)
-cy.get('input[id="Telegraf Database Name"]').clear().type(dbname)
-cy.get('input[id="Default Retention Policy"]').clear()
-}
-
-
-export const setupConnection = () => {
-  cy.visit('/')
-  // cy.waitFor('#tooltip', {timeout: 20000});
-  cy.get('h1', {timeout: 10000}).should('be.visible')
-  cy.get('h1').then($a => {
-    if ($a.text().includes('Welcome to Chronograf')) {
-      // initialize connections v2
-      cy.get('.wizard-button-bar').contains('Get Started').click()
-      createConnectionV2(
-        'http://localhost:9999',
-        'InfluxTest2',
-        'my-org',
-        'my-token',
-        'telegraf'
-      )
-      cy.get('.wizard-button-bar').contains('Add Connection').click()
-      cy.get('.notification-message').should(
-        'have.text',
-        'Connected to InfluxDB InfluxTest2 successfully.'
-      )
-      cy.get('.notification-close').click()
-      cy.get('button').contains('Next').click()
-      //kapacitor skip
-      cy.get('button').contains('Skip').click()
-      //setup complete
-      cy.get('button').contains('View All Connections').click()
-      cy.get('h1.page-header--title').should('have.text', 'Configuration')
-
-    }
+export const cutConnections = () => {
+  return cy
+    .request(
+      'GET',
+      '/chronograf/v1/sources'
+  )
+  .then(response => {
+    response.body.sources.forEach(connection => {
+      cy.request('DELETE', `${connection.links.self}`)      
+    })
   })
 }
 
-export const writeManualData = (
-  fieldKey: string,
-  dataValue: string) => {
-  cy.get('[data-test=write-data-button]').click()
-  cy.get('[data-test=dropdown-toggle]').first().click()
+export const createDashboard = (
+  name: string = 'Default Dashboard'
+) => {
+  cy.fixture('routes').then(({dashboards}) => {
+    return cy
+    .request(
+      {
+        method: 'POST',
+        url: `/chronograf/v1${dashboards}`,
+        body :{
+          name: name
+        }
+      })
+    .then(() => {
+      wrapDashboards()
+    })
+  }) 
+}
 
-  cy.get('[data-test="dropdown-item"]').filter(':contains("my-bucket")').click()
+export const deleteDashboards = () => {
+  return cy
+    .request(
+      'GET',
+      '/chronograf/v1/dashboards'
+    )
+    .then(({body: responseBody}) => {
+      responseBody.dashboards.forEach(dashboard => {
+        cy.request(
+          'DELETE',
+          `${dashboard.links.self}`
+        )
+      })
+    })
+}
 
-  cy.getByTitle('Write data manually using Line Protocol').click()
-  cy.get('[data-test=manual-entry-field]')
-    .clear()
-    .type(fieldKey + ' ' + dataValue)
-  cy.get('[data-test="write-data-submit-button"]', {timeout: 10000}).click()
-  cy.wait(2000)
+function wrapConnections(): Cypress.Chainable {
+  return cy
+    .request({
+      method: 'GET',
+      url: '/chronograf/v1/sources'
+    })
+    .then(response => {
+      const connections = response.body.sources
+      cy.wrap(connections).as('connections')
+    })  
+} 
+
+function wrapDashboards(): Cypress.Chainable {
+  return cy
+    .request(
+      'GET',
+      '/chronograf/v1/dashboards'
+    ).then(response => {
+      const dashboards = response.body.dashboards
+      cy.wrap(dashboards).as('dashboards')
+    })
 }
 
 Cypress.Commands.add('clickNav', clickNav)
