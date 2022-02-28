6defd734c9e3cf730d5d624cbdc8ee69ccdbc35c
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-02-28 22:25:09 +0100

    fix(ui): make influxQL query status robust

--- ui/src/shared/components/QueryStatus.tsx
+++ ui/src/shared/components/QueryStatus.tsx
@@ -36,19 +36,19 @@ class QueryStatus extends PureComponent<Props> {
       <div className="query-editor--status">
         <span
           className={classnames('query-status-output', {
-            'query-status-output--error': status.error,
-            'query-status-output--success': status.success,
-            'query-status-output--warning': status.warn,
+            'query-status-output--error': !!status.error,
+            'query-status-output--success': !!status.success,
+            'query-status-output--warning': !!status.warn,
           })}
         >
           <span
             className={classnames('icon', {
-              stop: status.error,
-              checkmark: status.success,
-              'alert-triangle': status.warn,
+              stop: !!status.error,
+              checkmark: !!status.success,
+              'alert-triangle': !!status.warn,
             })}
           />
-          {status.error || status.warn || status.success}
+          {String(status.error || status.warn || status.success)}
         </span>
         {children}
       </div>
