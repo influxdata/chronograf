8ed746a3ce5926b638f4c8521b7828df0cf1ccb4
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-03-11 10:20:44 +0100

    feat(ui/kapacitor): change Alerting main page to TICKscripts

--- ui/src/side_nav/containers/SideNav.tsx
+++ ui/src/side_nav/containers/SideNav.tsx
@@ -108,15 +108,15 @@ class SideNav extends PureComponent<Props> {
             'fluxtasks',
           ]}
           icon="alerts"
-          link={`${sourcePrefix}/alert-rules`}
+          link={`${sourcePrefix}/tickscripts`}
           location={location}
         >
-          <NavHeader link={`${sourcePrefix}/alert-rules`} title="Alerting" />
-          <NavListItem link={`${sourcePrefix}/alert-rules`}>
-            Alert Rules
-          </NavListItem>
+          <NavHeader link={`${sourcePrefix}/tickscripts`} title="Alerting" />
           <NavListItem link={`${sourcePrefix}/tickscripts`}>
             TICKscripts
+          </NavListItem>
+          <NavListItem link={`${sourcePrefix}/alert-rules`}>
+            Alert Rules
           </NavListItem>
           <NavListItem link={`${sourcePrefix}/flux-tasks`}>
             Flux Tasks
