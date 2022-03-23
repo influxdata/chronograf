c662f2c4368bb2400891c41974a200d79506622f
k3yi0 <definitelynotkeyio@gmail.com>
2022-03-23 12:56:43 +0100

    chore:  change file extenction to .ts and adjust code accordingly

--- ui/cypress/plugins/index.ts
+++ ui/cypress/plugins/index.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example plugins/index.ts can be used to load plugins
 //
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
@@ -11,18 +11,19 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin')
 
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
   // let's increase the browser window size when running headlessly
   // this will produce higher resolution images and videos
   // https://on.cypress.io/browser-launch-api
-  on('before:browser:launch', (browser = {}, launchOptions) => {
+  on('before:browser:launch', (browser: Cypress.Browser, launchOptions: Cypress.BrowserLaunchOptions) => {
     console.log(
       'launching browser %s is headless? %s',
       browser.name,
@@ -31,8 +32,8 @@ module.exports = (on, config) => {
 
     // the browser width and height we want to get
     // our screenshots and videos will be of that resolution
-    const width = 1920
-    const height = 1080
+    const width: number = 1920
+    const height: number = 1080
 
     console.log('setting the browser window size to %d x %d', width, height)
 
@@ -57,10 +58,6 @@ module.exports = (on, config) => {
     // IMPORTANT: return the updated browser launch options
     return launchOptions
   })
-}
 
-const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin')
-
-module.exports = (on, config) => {
   addMatchImageSnapshotPlugin(on, config)
 }
