78be030f78f40ce6bf82d38ea5431faa0067117e
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-03-25 13:12:41 +0100

    chore(ui): appease prettier

--- ui/test/shared/parsing/parseHandlersFromRule.test.js
+++ ui/test/shared/parsing/parseHandlersFromRule.test.js
@@ -13,8 +13,11 @@ describe('parseHandlersFromRule', () => {
   it('returns empty things if rule is new and config is empty', () => {
     const input1 = emptyRule
     const input2 = emptyConfigResponse
-    const {handlersOnThisAlert, selectedHandler, handlersOfKind} =
-      parseHandlersFromRule(input1, input2)
+    const {
+      handlersOnThisAlert,
+      selectedHandler,
+      handlersOfKind,
+    } = parseHandlersFromRule(input1, input2)
 
     expect(handlersOnThisAlert).toEqual([])
     expect(selectedHandler).toEqual(null)
@@ -24,8 +27,11 @@ describe('parseHandlersFromRule', () => {
   it('returns values if rule and config are not empty', () => {
     const input1 = rule
     const input2 = handlersFromConfig
-    const {handlersOnThisAlert, selectedHandler, handlersOfKind} =
-      parseHandlersFromRule(input1, input2)
+    const {
+      handlersOnThisAlert,
+      selectedHandler,
+      handlersOfKind,
+    } = parseHandlersFromRule(input1, input2)
 
     expect(handlersOnThisAlert).toEqual(handlersOnThisAlertExpected)
     expect(selectedHandler).toEqual(selectedHandlerExpected)
