a31bedfa3b19eea2e511bbbc60f7811df32a95e7
k3yi0 <definitelynotkeyio@gmail.com>
2022-04-26 13:21:43 +0200

    chore: auth2login test visit login page

--- ui/cypress/integration/oauth2login.test.ts
+++ ui/cypress/integration/oauth2login.test.ts
@@ -1,12 +1,13 @@
 describe('Log in and Log out using OAuth2 Mock Server', () => {
     it('use OAuth2 Mock Server', () => {
         cy.OAuthLogout()
+        cy.visit('/login')
         cy.wait(2000)
         cy.OAuthLogin('test')
         cy.wait(2000)
         cy.OAuthLogout()
         cy.wait(2000)
-        cy.OAuthLogin('test')
+        cy.OAuthLogin('Reader')
         cy.wait(2000)
     })
 })
