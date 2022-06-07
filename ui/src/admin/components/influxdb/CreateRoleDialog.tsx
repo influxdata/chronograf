5b702c0a50e404645afc15b0c4f2553c5fc50990
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-06-07 08:03:19 +0200

    feat(ui): add CreateRoleDIalog

---
+++ ui/src/admin/components/influxdb/CreateRoleDialog.tsx
@@ -0,0 +1,52 @@
+import React, {useCallback, useState} from 'react'
+import {
+  Form,
+  Input,
+  OverlayBody,
+  OverlayContainer,
+  OverlayHeading,
+  OverlayTechnology,
+} from 'src/reusable_ui'
+
+interface Props {
+  create: (role: {name: string}) => void
+  setVisible: (visible: boolean) => void
+  visible: boolean
+}
+const CreateRoleDialog = ({visible, setVisible, create}: Props) => {
+  const [name, setName] = useState('')
+  const cancel = useCallback(() => {
+    setName('')
+    setVisible(false)
+  }, [])
+  return (
+    <OverlayTechnology visible={visible}>
+      <OverlayContainer maxWidth={650}>
+        <OverlayHeading title="Create Role" onDismiss={cancel} />
+        <OverlayBody>
+          <Form>
+            <Form.Element label="Role Name">
+              <Input value={name} onChange={e => setName(e.target.value)} />
+            </Form.Element>
+            <Form.Footer>
+              <div className="form-group text-center form-group-submit col-xs-12">
+                <button className="btn btn-sm btn-default" onClick={cancel}>
+                  Cancel
+                </button>
+                <button
+                  className="btn btn-sm btn-success"
+                  disabled={!name}
+                  onClick={() => create({name})}
+                >
+                  Create
+                </button>
+              </div>
+            </Form.Footer>
+          </Form>
+        </OverlayBody>
+      </OverlayContainer>
+    </OverlayTechnology>
+  )
+}
+
+export default CreateRoleDialog
