b8717ff95530656406790d7160f2bf8a82863049
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-06-15 08:42:08 +0200

    chore(server): removed unused code

--- flux/client.go
+++ flux/client.go
@@ -1,15 +1,11 @@
 package flux
 
 import (
-	"context"
-	"errors"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
 	"time"
 
-	"github.com/influxdata/chronograf"
 	"github.com/influxdata/chronograf/util"
 )
 
@@ -24,32 +20,6 @@ type Client struct {
 	URL                *url.URL
 	InsecureSkipVerify bool
 	Timeout            time.Duration
-}
-
-// Ping checks the connection of a Flux.
-func (c *Client) Ping(ctx context.Context) error {
-	t := 2 * time.Second
-	if c.Timeout > 0 {
-		t = c.Timeout
-	}
-	ctx, cancel := context.WithTimeout(ctx, t)
-	defer cancel()
-	err := c.pingTimeout(ctx)
-	return err
-}
-
-func (c *Client) pingTimeout(ctx context.Context) error {
-	resps := make(chan (error))
-	go func() {
-		resps <- c.ping(c.URL)
-	}()
-
-	select {
-	case resp := <-resps:
-		return resp
-	case <-ctx.Done():
-		return chronograf.ErrUpstreamTimeout
-	}
 }
 
 // FluxEnabled returns true if the server has flux querying enabled.
@@ -82,37 +52,4 @@ func (c *Client) FluxEnabled() (bool, error) {
 	// 2.x: Flux is always enabled, the 401 response with 'application/json; charset=utf-8' content type and body
 	// {"code":"unauthorized","message":"unauthorized access"} is received
 	return strings.HasPrefix(contentType, "application/json"), nil
-}
-
-func (c *Client) ping(u *url.URL) error {
-	u = util.AppendPath(u, "/ping")
-
-	req, err := http.NewRequest("GET", u.String(), nil)
-	if err != nil {
-		return err
-	}
-
-	hc := &http.Client{}
-	if c.InsecureSkipVerify {
-		hc.Transport = skipVerifyTransport
-	} else {
-		hc.Transport = defaultTransport
-	}
-
-	resp, err := hc.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusNoContent {
-		return errors.New(string(body))
-	}
-
-	return nil
 }
