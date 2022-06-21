96d2641850cc928db1048d47de7bf2e35a2a86e9
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-06-21 14:39:15 +0200

    chore: improve docs

--- util/path.go
+++ util/path.go
@@ -4,7 +4,7 @@ import (
 	"net/url"
 )
 
-// AppendPath appends path to the supplied URL and returns a new URL instance.
+// AppendPath returns a new URL that appends path to the supplied URL
 func AppendPath(url *url.URL, path string) *url.URL {
 	retVal := *url
 	if len(path) == 0 {
