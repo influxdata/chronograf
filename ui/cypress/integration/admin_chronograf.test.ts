bcfd70687a517b8b27ea3d3c75ee0f5f64bd85fe
k3yi0 <definitelynotkeyio@gmail.com>
2022-06-21 16:27:56 +0200

    test(cypress): add All Orgs test

--- ui/cypress/integration/admin_chronograf.test.ts
+++ ui/cypress/integration/admin_chronograf.test.ts
@@ -80,4 +80,62 @@ describe('Chronograf', () => {
       })
     })
   })
+
+  describe('All Orgs', () => {
+    beforeEach(() => {
+      cy.visit(url + '/all-organizations')
+    })
+
+    it.only('create an org, edit it, and delete it', () => {
+      cy.getByTestID('create-new-org--button').click()
+      cy.getByTestID('cancel').click()
+      cy.getByTestID(
+        `${chronograf.organizations[0].name}-org--row`
+      ).should('not.exist')
+
+      cy.getByTestID('create-new-org--button').click()
+      cy.getByTestID('new-org--row').within(() => {
+        cy.getByTestID('new-org-name--input')
+          .clear()
+          .type(chronograf.organizations[0].name)
+
+        cy.get('.dropdown-selected').click()
+        cy.getByTestID(
+          `${chronograf.organizations[0].defaultRole}-dropdown-item`
+        ).click()
+        cy.get('.dropdown-selected').should('contain.text', chronograf.organizations[0].defaultRole)
+
+        cy.getByTestID('confirm').click()
+      })
+
+      cy.getByTestID(`${chronograf.organizations[0].name}-org--row`)
+        .should('exist')
+        .within(() => {
+          cy.getByTestID(`${chronograf.organizations[0].name}-org-name`)
+            .should('exist')
+            .and('contain.text', chronograf.organizations[0].name)
+            .then($el => {
+              if (Cypress.dom.isDetached($el)) cy.wait(500);
+              Cypress.$($el).trigger('click')
+            });
+
+          cy.getByTestID('rename-org--input')
+            .should('exist')
+            .clear()
+            .type(chronograf.organizations[1].name + '{enter}')
+          cy.getByTestID(
+            `${chronograf.organizations[1].name}-org-name`
+          ).should('contain.text', chronograf.organizations[1].name)
+
+          cy.get('.dropdown-selected').click()
+          cy.getByTestID(`${chronograf.organizations[1].defaultRole}-dropdown-item`
+          ).click()
+          cy.get('.dropdown-selected').should('contain.text', chronograf.organizations[1].defaultRole)
+          cy.getByTestID('delete-org--button').click()
+            
+          cy.getByTestID(`${chronograf.organizations[1].name}-org--row`)
+          .should('not.exist')
+        })
+    })
+  })
 })
