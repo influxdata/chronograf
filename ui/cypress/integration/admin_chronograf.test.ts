0f510a4b75f2c2ca21a2b28114cc2b414486bd59
k3yi0 <definitelynotkeyio@gmail.com>
2022-06-22 07:47:26 +0200

    chore(cypress): remove only

--- ui/cypress/integration/admin_chronograf.test.ts
+++ ui/cypress/integration/admin_chronograf.test.ts
@@ -88,7 +88,7 @@ describe('Chronograf', () => {
       cy.visit(url + '/all-organizations')
     })
 
-    it.only('create an org, edit it, and delete it', () => {
+    it('create an org, edit it, and delete it', () => {
       cy.getByTestID('create-new-org--button').click()
       cy.getByTestID('cancel').click()
       cy.getByTestID(
