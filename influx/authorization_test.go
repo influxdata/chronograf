b1988c832a3333419f88035c566ea7fa8c70ad2b
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-07-01 06:43:31 +0200

    chore(server): add test for JWT token with non-zero time

--- influx/authorization_test.go
+++ influx/authorization_test.go
@@ -28,6 +28,17 @@ func TestJWT(t *testing.T) {
 			},
 			want: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjYwLCJ1c2VybmFtZSI6IkF6dXJlRGlhbW9uZCJ9.kUWGwcpCPwV7MEk7luO1rt8036LyvG4bRL_CfseQGmz4b0S34gATx30g4xvqVAV6bwwYE0YU3P8FjG8ij4kc5g",
 		},
+		{
+			name: "",
+			args: args{
+				username:     "Pepa",
+				sharedSecret: "hunter2",
+				now: func() time.Time {
+					return time.Date(2022, time.July, 1, 20, 0, 0, 0, time.UTC)
+				},
+			},
+			want: "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTY3MDU2NjAsInVzZXJuYW1lIjoiUGVwYSJ9.zrY8LHP4jjZ-MaNOLEeN5dPC8SdA132DjhHv83dHF48lHV25u6OfFKEW11hZlVdI7f6z_bGxTMMezKHnf04jYQ",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
