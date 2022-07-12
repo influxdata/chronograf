524d2b87b1949b90bce1158194eedb88ce3f5630
k3yi0 <definitelynotkeyio@gmail.com>
2022-07-12 12:40:49 +0200

    chore(cypress): remove only

--- ui/cypress/integration/dashboard.test.ts
+++ ui/cypress/integration/dashboard.test.ts
@@ -43,7 +43,7 @@ describe('Use Dashboards', () => {
       })
     })
 
-    it.only('ensure that all elements used to edit Chronograf are not visible', () => {
+    it('ensure that all elements used to edit Chronograf are not visible', () => {
       cy.url().should('match', new RegExp(`sources/${source.id}/dashboards`))
       cy.getByTestID('sidebar').should('not.exist')
       cy.getByTestID('import-dashboard--button').should('not.exist')
