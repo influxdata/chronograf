539766538702182b4734ec00656168d26c8b0a96
Pavel Zavora <pavel.zavora@bonitoo.io>
2022-07-22 11:39:34 +0200

    chore: upgrade jest

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
           tsconfig: './tsconfig.test.json',
         },
       },
+      testEnvironment: 'jsdom',
       displayName: 'test',
       testPathIgnorePatterns: [
         'build',
