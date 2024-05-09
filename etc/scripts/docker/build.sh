8bbdca5cda14433185e90bc852d9bc90b4af6ffe
Jakub Bednar <jakub.bednar@gmail.com>
2024-05-09 11:43:39 +0200

    chore(ci): use platform corresponding with CI

--- etc/scripts/docker/build.sh
+++ etc/scripts/docker/build.sh
@@ -3,7 +3,7 @@ set -x
 
 DOCKER_TAG="chronograf-$(date +%Y%m%d)"
 
-docker build --rm=false -f etc/Dockerfile_build -t builder:$DOCKER_TAG .
+docker build --rm=false --platform linux/amd64 -f etc/Dockerfile_build -t builder:$DOCKER_TAG .
 docker tag builder:$DOCKER_TAG quay.io/influxdb/builder:$DOCKER_TAG
 
 docker push quay.io/influxdb/builder:$DOCKER_TAG
