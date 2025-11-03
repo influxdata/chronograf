8243c94bf9441cd7ecbf43e7f06059dcb2a6e383
vlastahajek <29980246+vlastahajek@users.noreply.github.com>
2025-11-03 11:17:10 +0100

    chore: refactored WizardDropdown to functional  component

--- ui/src/reusable_ui/components/wizard/WizardDropdown.tsx
+++ ui/src/reusable_ui/components/wizard/WizardDropdown.tsx
@@ -1,7 +1,6 @@
-import React, {PureComponent} from 'react'
+import React, {useMemo, useCallback} from 'react'
 import Dropdown from 'src/shared/components/Dropdown'
 import {DropdownItem} from 'src/types'
-import {ErrorHandling} from 'src/shared/decorators/errors'
 
 interface DropdownOption {
   value: string
@@ -19,44 +18,52 @@ interface Props {
   testId?: string
 }
 
-class WizardDropdown extends PureComponent<Props> {
-  public render() {
-    const {label, subtext, halfWidth, testId} = this.props
+const WizardDropdown: React.FC<Props> = ({
+  value,
+  options,
+  placeholder = 'Select option...',
+  label,
+  subtext,
+  onChange,
+  halfWidth,
+  testId,
+}) => {
+  const dropdownItems = useMemo(
+    () =>
+      options.map(option => ({
+        text: option.label,
+      })),
+    [options]
+  )
 
-    return (
-      <div className={'form-group ' + (halfWidth ? 'col-xs-6' : 'col-xs-12')}>
-        <label htmlFor={`wizard-dropdown-${testId}`}>{label}</label>
-        <Dropdown
-          items={this.dropdownItems}
-          selected={this.selectedText}
-          onChoose={this.handleChoose}
-          className="dropdown-stretch"
-          data-testid={testId}
-        />
-        {subtext && <span className="wizard-dropdown--subtext">{subtext}</span>}
-      </div>
-    )
-  }
-
-  private get dropdownItems(): DropdownItem[] {
-    return this.props.options.map(option => ({
-      text: option.label,
-    }))
-  }
-
-  private get selectedText(): string {
-    const {value, options, placeholder = 'Select option...'} = this.props
+  const selectedText = useMemo(() => {
     const selectedOption = options.find(opt => opt.value === value)
     return selectedOption ? selectedOption.label : placeholder
-  }
+  }, [value, options, placeholder])
 
-  private handleChoose = (item: DropdownItem) => {
-    const {options, onChange} = this.props
-    const selectedOption = options.find(opt => opt.label === item.text)
-    if (selectedOption) {
-      onChange(selectedOption.value)
-    }
-  }
+  const handleChoose = useCallback(
+    (item: DropdownItem) => {
+      const selectedOption = options.find(opt => opt.label === item.text)
+      if (selectedOption) {
+        onChange(selectedOption.value)
+      }
+    },
+    [options, onChange]
+  )
+
+  return (
+    <div className={'form-group ' + (halfWidth ? 'col-xs-6' : 'col-xs-12')}>
+      <label htmlFor={`wizard-dropdown-${testId}`}>{label}</label>
+      <Dropdown
+        items={dropdownItems}
+        selected={selectedText}
+        onChoose={handleChoose}
+        className="dropdown-stretch"
+        data-testid={testId}
+      />
+      {subtext && <span className="wizard-dropdown--subtext">{subtext}</span>}
+    </div>
+  )
 }
 
-export default ErrorHandling(WizardDropdown)
+export default WizardDropdown
