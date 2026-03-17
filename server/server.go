c9cd31d0abb185ab161f7ef7899e8c8da22df7ad
Ales Pour <ales.pour@bonitoo.io>
2026-03-17 10:38:25 +0100

    fix: cookie hardening

--- server/server.go
+++ server/server.go
@@ -772,7 +772,7 @@ func (s *Server) Serve(ctx context.Context) {
 	transport.TLSClientConfig.RootCAs = certs
 	s.oauthClient = http.Client{Transport: transport}
 
-	auth := oauth2.NewCookieJWT(s.TokenSecret, s.AuthDuration, s.InactivityDuration)
+	auth := oauth2.NewCookieJWT(s.TokenSecret, s.AuthDuration, s.InactivityDuration, s.useTLS())
 	providerFuncs := []func(func(oauth2.Provider, oauth2.Mux)){
 		provide(s.githubOAuth(logger, auth)),
 		provide(s.googleOAuth(logger, auth)),
