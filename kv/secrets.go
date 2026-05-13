10c6c3c52207d0d69306d55eef2cc59a9c0d83bb
Ales Pour <ales.pour@bonitoo.io>
2026-05-13 12:28:00 +0200

    feat(kv): bootstrap wrapped DEK and load secret DEK on startup

---
+++ kv/secrets.go
@@ -0,0 +1,187 @@
+package kv
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/influxdata/chronograf"
+	"github.com/influxdata/chronograf/kv/internal"
+	"google.golang.org/protobuf/proto"
+)
+
+var wrappedDEKID = []byte("secrets/wrapped-dek/v1")
+
+// InitializeSecretDEK loads or creates a wrapped DEK in KV and configures
+// kv/internal secret encryption for this process.
+func (s *Service) InitializeSecretDEK(ctx context.Context, masterKey []byte) error {
+	// Fail closed across restarts/reconfigurations in the same process.
+	internal.SetSecretDEK(nil)
+
+	var wrappedDEK []byte
+	if err := s.kv.View(ctx, func(tx Tx) error {
+		v, err := tx.Bucket(configBucket).Get(wrappedDEKID)
+		if err != nil {
+			return err
+		}
+		if len(v) > 0 {
+			wrappedDEK = append([]byte(nil), v...)
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	if len(wrappedDEK) == 0 {
+		if len(masterKey) == 0 {
+			return nil
+		}
+
+		dek, err := internal.GenerateDEK()
+		if err != nil {
+			return err
+		}
+		wrapped, err := internal.WrapDEK(masterKey, dek)
+		if err != nil {
+			return err
+		}
+
+		if err := s.kv.Update(ctx, func(tx Tx) error {
+			return tx.Bucket(configBucket).Put(wrappedDEKID, wrapped)
+		}); err != nil {
+			return err
+		}
+
+		internal.SetSecretDEK(dek)
+		if err := s.migrateLegacyPlaintextSecrets(ctx); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if len(masterKey) == 0 {
+		return errors.New("wrapped DEK exists but no secrets master key is configured")
+	}
+
+	dek, err := internal.UnwrapDEK(masterKey, wrappedDEK)
+	if err != nil {
+		return fmt.Errorf("unable to unwrap stored DEK: %w", err)
+	}
+
+	internal.SetSecretDEK(dek)
+	if err := s.migrateLegacyPlaintextSecrets(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Service) migrateLegacyPlaintextSecrets(ctx context.Context) error {
+	return s.kv.Update(ctx, func(tx Tx) error {
+		if err := migrateSourcesBucket(tx); err != nil {
+			return err
+		}
+		if err := migrateServersBucket(tx); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func migrateSourcesBucket(tx Tx) error {
+	b := tx.Bucket(sourcesBucket)
+
+	var rewrites []struct {
+		key []byte
+		val []byte
+	}
+
+	if err := b.ForEach(func(k, v []byte) error {
+		var pb internal.Source
+		if err := proto.Unmarshal(v, &pb); err != nil {
+			return err
+		}
+		if !sourceNeedsSecretMigration(pb) {
+			return nil
+		}
+
+		var src chronograf.Source
+		if err := internal.UnmarshalSource(v, &src); err != nil {
+			return err
+		}
+		migrated, err := internal.MarshalSource(src)
+		if err != nil {
+			return err
+		}
+
+		rewrites = append(rewrites, struct {
+			key []byte
+			val []byte
+		}{
+			key: append([]byte(nil), k...),
+			val: migrated,
+		})
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	for _, rw := range rewrites {
+		if err := b.Put(rw.key, rw.val); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func migrateServersBucket(tx Tx) error {
+	b := tx.Bucket(serversBucket)
+
+	var rewrites []struct {
+		key []byte
+		val []byte
+	}
+
+	if err := b.ForEach(func(k, v []byte) error {
+		var pb internal.Server
+		if err := proto.Unmarshal(v, &pb); err != nil {
+			return err
+		}
+		if pb.Password == "" || pb.GetPasswordEncoding() == internal.SecretEncoding_ENCRYPTED_V1 {
+			return nil
+		}
+
+		var srv chronograf.Server
+		if err := internal.UnmarshalServer(v, &srv); err != nil {
+			return err
+		}
+		migrated, err := internal.MarshalServer(srv)
+		if err != nil {
+			return err
+		}
+
+		rewrites = append(rewrites, struct {
+			key []byte
+			val []byte
+		}{
+			key: append([]byte(nil), k...),
+			val: migrated,
+		})
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	for _, rw := range rewrites {
+		if err := b.Put(rw.key, rw.val); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func sourceNeedsSecretMigration(pb internal.Source) bool {
+	return (pb.Password != "" && pb.GetPasswordEncoding() != internal.SecretEncoding_ENCRYPTED_V1) ||
+		(pb.SharedSecret != "" && pb.GetSharedSecretEncoding() != internal.SecretEncoding_ENCRYPTED_V1) ||
+		(pb.ManagementToken != "" && pb.GetManagementTokenEncoding() != internal.SecretEncoding_ENCRYPTED_V1) ||
+		(pb.DatabaseToken != "" && pb.GetDatabaseTokenEncoding() != internal.SecretEncoding_ENCRYPTED_V1)
+}
